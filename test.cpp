3835a424c7dfb9189ed97b72297101990be4e697
Lee Jong Hyeok <gurpaper@naver.com>
2024-07-12 13:07:21 +0900

    1996번 프린터 큐

--- test.cpp
+++ test.cpp
@@ -1,7 +1,6 @@
 	#include <iostream>
-	#include <vector>
-	#include <algorithm>
-	#include <cmath>
+	#include <queue>
+	#include <array>
 
 	using namespace std;
 	int main()
@@ -9,24 +8,61 @@
 		std::ios::sync_with_stdio(false);
 		std::cin.tie(NULL); 
 		std::cout.tie(NULL);
-		vector<int> v;
-		int N;
-		cin >> N;
-		if (N == 0) {
-			cout << "0";
-		}
-		else {
+		int K;
+		cin >> K;
+		int N, M;
+		for (int i = 0; i < K; ++i) {
+			cin >> N >> M;
+			queue<int> q;
+			array<int, 9> arr{ 0 };
+			int max = 0;
+			int my_pro;
 			for (int i = 0; i < N; ++i) {
-				int a;
-				cin >> a;
-				v.emplace_back(a);
+				int num;
+				cin >> num;
+				q.push(num);
+				if (max < num)
+					max = num;
+				if (i == M)
+					my_pro = num;
+				arr[num - 1]++;
 			}
-			std::sort(v.begin(), v.end());
-			int erase_size = std::lround(N * 0.15);
-			int sum = 0;
-			for (int i = erase_size; i < N - erase_size; ++i) {
-				sum += v[i];
+			int index = M;
+			int count = 1;
+			while (true) {
+				int pro = q.front();
+				if (index == 0) {
+					if (max == my_pro) {
+						cout << count << "\n";
+						break;
+					}
+					else {
+						q.pop();
+						q.push(pro);
+						index = q.size()-1;
+						continue;
+					}
+				}
+				if (pro == max) {
+					q.pop();
+					arr[max - 1]--;
+					if (arr[max - 1] == 0) {
+						for (int i = 8; i >=0; --i) {
+							if (arr[i] > 0) {
+								max = i + 1;
+								break;
+							}
+						}
+					}
+					count++;
+					index--;
+				}
+				else {
+					q.pop();
+					q.push(pro);
+					index--;
+				}
+
 			}
-			cout << std::lround(float(sum) / (N - (erase_size * 2)));
 		}
 	}
