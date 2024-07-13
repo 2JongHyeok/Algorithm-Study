8a38d0f5dca57ac50d9e9ab6ee817c7c837f33c2
2JongHyeok <gurpaper@naver.com>
2024-07-13 12:54:33 +0900

    1654번 랜선 자르기

--- test.cpp
+++ test.cpp
@@ -1,68 +1,42 @@
-	#include <iostream>
-	#include <queue>
-	#include <array>
+#include <iostream>
+#include <queue>
+#include <array>
 
-	using namespace std;
-	int main()
-	{
-		std::ios::sync_with_stdio(false);
-		std::cin.tie(NULL); 
-		std::cout.tie(NULL);
-		int K;
-		cin >> K;
-		int N, M;
-		for (int i = 0; i < K; ++i) {
-			cin >> N >> M;
-			queue<int> q;
-			array<int, 9> arr{ 0 };
-			int max = 0;
-			int my_pro;
-			for (int i = 0; i < N; ++i) {
-				int num;
-				cin >> num;
-				q.push(num);
-				if (max < num)
-					max = num;
-				if (i == M)
-					my_pro = num;
-				arr[num - 1]++;
-			}
-			int index = M;
-			int count = 1;
-			while (true) {
-				int pro = q.front();
-				if (index == 0) {
-					if (max == my_pro) {
-						cout << count << "\n";
-						break;
-					}
-					else {
-						q.pop();
-						q.push(pro);
-						index = q.size()-1;
-						continue;
-					}
-				}
-				if (pro == max) {
-					q.pop();
-					arr[max - 1]--;
-					if (arr[max - 1] == 0) {
-						for (int i = 8; i >=0; --i) {
-							if (arr[i] > 0) {
-								max = i + 1;
-								break;
-							}
-						}
-					}
-					count++;
-					index--;
-				}
-				else {
-					q.pop();
-					q.push(pro);
-					index--;
-				}
+using namespace std;
 
-			}
-		}
+int main()
+{
+	std::ios::sync_with_stdio(false);
+	std::cin.tie(NULL);
+	std::cout.tie(NULL);
+	int K, N;
+	cin >> K >> N;
+	vector<int>v;
+	v.reserve(K);
+	int length = 0;
+	int min=1; 
+	unsigned int max = 0x7FFFFFFF;
+	for (int i = 0; i < K; ++i) {
+		int num;
+		cin >> num;
+		v.emplace_back(num);
 	}
+	unsigned int mid;
+	while (true) {
+		mid = (max +min)/2;
+		int sum = 0;
+		for (int a : v) {
+			sum += (a / mid);
+		}
+		if (sum < N) {
+			max = mid-1;
+		}
+		else if (sum >= N) {
+			min = mid+1;
+		}
+		if (min > max)
+			break;
+	}
+	
+	cout << max;
+}
