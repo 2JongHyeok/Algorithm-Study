f029ea8e36f209164e379c60842af34d98d3ad24
Lee Jong Hyeok <gurpaper@naver.com>
2024-07-15 11:38:26 +0900

    11723번 집합

--- test.cpp
+++ test.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <unordered_map>
 #include <string>
 
 using namespace std;
@@ -9,27 +8,41 @@ int main()
 	std::ios::sync_with_stdio(false);
 	std::cin.tie(NULL);
 	std::cout.tie(NULL);
-	int M,N;
-	cin >> M>>N;
-	unordered_map<string, int> m;
-	unordered_map<int,string> m2;
+	int M;
+	cin >> M;
+	int result = 0;
+	string s;
+	int a;
+	for (int i = 0; i < M; ++i) {
+		cin >> s;
+		if (s == "add") {
+			cin >> a;
+			result |= (1 << a);
+		}
+		else if (s == "remove") {
+			cin >> a;
+			result &= ~(1 << a);
+		}
+		else if (s == "check") {
+			cin >> a;
+			if (result & (1 << a)) {
+				cout << "1\n";
+			}
+			else {
+				cout << "0\n";
+			}
+		}
+		else if (s == "toggle") {
+			cin >> a;
+			result ^= (1 << a);
+		}
+		else if (s == "all") {
+			result = (1 << 21) - 1;
+		}
+		else if (s == "empty") {
+			result = 0;
+		}
 
-	for (int i = 1; i <= M; ++i) {
-		string s;
-		cin >> s;
-		m[s]=i;
-		m2[i] = s;
 	}
-	for (int i = 0; i < N; ++i) {
-		string s;
-		int a;
-		cin >> s;
-		if (s[0] >= 'A') {
-			cout << m[s]<<"\n";
-		}
-		else {
-			a = stoi(s);
-			cout << m2[a] << "\n";
-		}
-	}
+	
 }
