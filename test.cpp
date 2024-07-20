9c656096f6cf4be7da5b023f29b81d3e07709565
2JongHyeok <gurpaper@naver.com>
2024-07-20 14:22:30 +0900

    1463번 1로 만들기

--- test.cpp
+++ test.cpp
@@ -3,7 +3,7 @@
 
 using namespace std;
 
-int arr[100001];
+int arr[1000001];
 
 int main()
 {
@@ -16,10 +16,10 @@ int main()
 	for (int i = 2; i <= N; ++i) {
 		arr[i] = arr[i - 1] + 1;
 		if (i % 2 == 0) {
-			arr[i] =min(arr[i], arr[i / 2] + 1);
+			arr[i] = min(arr[i], arr[i / 2] + 1);
 		}
 		if (i % 3 == 0) {
-			arr[i] =min(arr[i], arr[i / 3] + 1);
+			arr[i] = min(arr[i], arr[i / 3] + 1);
 		}
 	}
 	cout << arr[N];	
