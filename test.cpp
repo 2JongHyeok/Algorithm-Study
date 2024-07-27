b9f0748d61c065ac8b4a3f641d6c5d127b391d7f
2JongHyeok <gurpaper@naver.com>
2024-07-27 17:45:14 +0900

    11659번 구간 합 구하기 4

--- test.cpp
+++ test.cpp
@@ -3,32 +3,24 @@
 #include <cmath>
 
 using namespace std;
-
-long long dp[101];
+int arr[100'001];
 
 int main()
 {
 	std::ios::sync_with_stdio(false);
 	std::cin.tie(NULL);
 	std::cout.tie(NULL);
-	int T, N;
-	cin >> T;
-	dp[1] = 1;
-	dp[2] = 1;
-	dp[3] = 1;
-	dp[4] = 2;
-	dp[5] = 2;
-	dp[6] = 3;
-	dp[7] = 4;
-	dp[8] = 5;
-	dp[9] = 7;
-	dp[10] = 9;
-	for (int i = 11; i <= 100; ++i) {
-		dp[i] = dp[i - 1] + dp[i - 5];
+	int N, M;
+	cin >> N >> M;
+	int num;
+	arr[0] = 0;
+	for (int i = 1; i <= N; ++i) {
+		cin >> num;
+		arr[i] = arr[i - 1] + num;
 	}
-	for (int i = 0; i < T; ++i) {
-		cin >> N;
-		cout << dp[N] << "\n";
+	int start, end;
+	for (int i = 0; i < M; ++i) {
+		cin >> start >> end;
+		cout << arr[end] - arr[start-1] << "\n";
 	}
-
 }
