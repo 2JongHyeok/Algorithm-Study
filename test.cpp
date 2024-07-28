0ef201670a429613773e3fa82e688c394a16449f
Lee Jong Hyeok <gurpaper@naver.com>
2024-07-28 18:37:50 +0900

    11726번 2Xn 타일링

--- test.cpp
+++ test.cpp
@@ -3,24 +3,23 @@
 #include <cmath>
 
 using namespace std;
-int arr[100'001];
+long long arr[1001];
 
 int main()
 {
 	std::ios::sync_with_stdio(false);
 	std::cin.tie(NULL);
 	std::cout.tie(NULL);
-	int N, M;
-	cin >> N >> M;
-	int num;
-	arr[0] = 0;
-	for (int i = 1; i <= N; ++i) {
-		cin >> num;
-		arr[i] = arr[i - 1] + num;
+	int N;
+	cin >> N;
+	arr[1] = 1;
+	arr[2] = 2;
+	arr[3] = 3;
+	arr[4] = 5;
+	arr[5] = 8;
+	for (int i = 6; i <= N; ++i) {
+		arr[i] = arr[i - 1]%10007 + arr[i - 2]%10007;
+		arr[i] %= 10007;
 	}
-	int start, end;
-	for (int i = 0; i < M; ++i) {
-		cin >> start >> end;
-		cout << arr[end] - arr[start-1] << "\n";
-	}
+	cout << arr[N];
 }
