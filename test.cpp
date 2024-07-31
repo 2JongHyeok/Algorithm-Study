ee8f1d42377934664e8721e03f98f2e70166bbdc
Lee Jong Hyeok <gurpaper@naver.com>
2024-07-31 16:26:15 +0900

    1260번 DFS와 BFS

--- test.cpp
+++ test.cpp
@@ -1,25 +1,54 @@
 #include <iostream>
-#include <algorithm>
+#include <stack>
+#include <queue>
+#include <set>
+#include <unordered_map>
 
 using namespace std;
-int dp[50'001];
+unordered_map<int, set<int>> umap ;
+unordered_map<int, int> dfs;
+unordered_map<int, int> bfs;
+
+void do_dfs(int v) {
+	if (dfs[v] == 1)
+		return;
+	dfs[v] = 1;
+	cout << v << " ";
+	for (auto n : umap[v]) {
+		do_dfs(n);
+	}
+}
 
 int main()
 {
 	std::ios::sync_with_stdio(false);
 	std::cin.tie(NULL);
 	std::cout.tie(NULL);
-
-	int N;
-	cin >> N;
-	dp[1] = 1;
-	for (int i = 2; i <= N; ++i) {
-		int mini = 50000;
-		for(int j = 1; j * j <= i; ++j) {
-			int tmp = i - j * j;
-			mini = min(mini, dp[tmp]);
-		}
-		dp[i] = mini + 1;
+	queue<int> q;
+	int N, V, M;
+	cin >> N >> M >> V;
+	int start, end;
+	for (int i = 0; i < M; ++i) {
+		cin >> start >> end;
+		umap[start].insert(end);
+		umap[end].insert(start);
 	}
-	cout << dp[N];
+
+	do_dfs(V);
+	cout << "\n";
+
+	q.push(V);
+	while (!q.empty()) {
+		int node = q.front();
+		q.pop();
+		if (bfs[node] == 1)
+			continue;
+		cout << node << " ";
+		bfs[node] = 1;
+		for (int n : umap[node]) {
+			q.push(n);
+		}
+	}
+	cout << "\n";
+
 }
