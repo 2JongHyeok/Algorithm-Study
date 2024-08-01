9a54a0cc9387c56a5b0340185de9e3db0d7448a0
Lee Jong Hyeok <gurpaper@naver.com>
2024-08-01 16:13:47 +0900

    1927번 최소 힙

--- test.cpp
+++ test.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
-#include <string>
+#include <queue>
+#include <vector>
 
 using namespace std;
 
@@ -8,42 +9,23 @@ int main()
 	std::ios::sync_with_stdio(false);
 	std::cin.tie(NULL);
 	std::cout.tie(NULL);
-	string s;
-	cin >> s;
-	int num[3]{ 0 };
-	int now = 0;
-	int plus = 1;
-	bool minus = false;
-	for (char c : s) {
-		if (c == '+') {
-			num[1] += num[2];
-			num[2] = 0;
-		}
-		else if (c == '-') {
-			num[1] += num[2];
-			num[2] = 0;
-			if (minus) {
-				num[0] -= num[1];
-				num[1] = 0;
+	int N;
+	cin >> N;
+	std::priority_queue<int, std::vector<int>, std::greater<int>> min_heap;
+	int num;
+	for (int i = 0; i < N; ++i) {
+		cin >> num;
+		if (num == 0) {
+			if (min_heap.size() == 0) {
+				cout << "0\n";
+				continue;
 			}
-			else {
-				num[0] = num[1];
-				num[1] = 0;
-				minus = true;
-			}
+			cout << min_heap.top() << "\n";
+			min_heap.pop();
 		}
 		else {
-			num[2] *= 10;
-			num[2] += int(c-'0');
+			min_heap.push(num);
 		}
 	}
-	num[1] += num[2];
-	if (minus) {
-		num[2] = 0;
-		num[0] -= num[1];
-	}
-	else {
-		num[0] = num[1];
-	}
-	cout << num[0];
+	
 }
