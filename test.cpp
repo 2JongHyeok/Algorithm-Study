111400596a1c74c874016e8aa4cdff733478abe6
Lee Jong Hyeok <gurpaper@naver.com>
2024-08-02 11:22:10 +0900

    2630번 색종이 만들기

--- test.cpp
+++ test.cpp
@@ -1,8 +1,38 @@
 #include <iostream>
-#include <queue>
-#include <vector>
 
 using namespace std;
+
+int white = 0;
+int blue = 0;
+
+int paper[128][128];
+
+void one_color(int startX, int endX, int startY, int endY) {
+	int first_color = paper[startY][startX];
+	bool same = true;
+	for (int i = startY; i < endY; ++i) {
+		for (int j = startX; j < endX; ++j) {
+			if (first_color != paper[i][j]) {
+				same = false;
+				break;
+			}
+		}
+		if (!same) break;
+	}
+	if (!same) {
+		one_color(startX, (startX+endX)/2, startY,(startY+endY)/2);
+		one_color((startX + endX) / 2, endX, startY, (startY + endY) / 2);
+		one_color(startX, (startX + endX) / 2, (startY + endY) / 2, endY);
+		one_color((startX + endX) / 2, endX, (startY + endY) / 2, endY);
+	}
+	else {
+		if (first_color == 1) {
+			blue++;
+		}
+		else
+			white++;
+	}
+}
 
 int main()
 {
@@ -11,21 +41,12 @@ int main()
 	std::cout.tie(NULL);
 	int N;
 	cin >> N;
-	std::priority_queue<int, std::vector<int>, std::greater<int>> min_heap;
-	int num;
 	for (int i = 0; i < N; ++i) {
-		cin >> num;
-		if (num == 0) {
-			if (min_heap.size() == 0) {
-				cout << "0\n";
-				continue;
-			}
-			cout << min_heap.top() << "\n";
-			min_heap.pop();
-		}
-		else {
-			min_heap.push(num);
+		for (int j = 0; j < N; ++j) {
+			cin >> paper[i][j];
 		}
 	}
-	
+	one_color(0, N, 0, N);
+	cout << white << "\n";
+	cout << blue;
 }
