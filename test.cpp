def7c9b47b5cc35d906238a8cddb702c328e4ba8
2JongHyeok <gurpaper@naver.com>
2024-08-05 02:19:05 +0900

    18111번 마인크래프트

--- test.cpp
+++ test.cpp
@@ -1,15 +1,8 @@
 #include <iostream>
-#include <array>
-#include <vector>
-#include <unordered_set>
-#include <queue>
+
 
 using namespace std;
 
-array<vector<int>, 1001> graph;
-unordered_set<int> CC;
-
-queue<int> Component;
 
 int main()
 {
@@ -17,33 +10,40 @@ int main()
 	std::cin.tie(NULL);
 	std::cout.tie(NULL);
 	
-	int N, M;
-	cin >> N >>M;
-	int start, end;
-	for (int i = 0; i < M; ++i) {
-		cin >> start >> end;
-		graph[start].emplace_back(end);
-		graph[end].emplace_back(start);
+	int N, M, B;
+	cin >> N >> M >> B;
+	int* arr = new int[N * M];
+	int total = 0;
+	int max = 0;
+	for (int i = 0; i < N * M; ++i) {
+		cin >> arr[i];
+		total += arr[i];
+		if (arr[i] > max)
+			max = arr[i];
 	}
-	int result = 0;
-	bool first = true;
-	for (int i = 1; i <= N; ++i) {
-		if (CC.find(i) == CC.end()) {
-			result++;
-			Component.push(i);
-		}
-		else continue;
-
-		while (!Component.empty()) {
-			int num = Component.front();
-			Component.pop();
-			if (CC.find(num) != CC.end()) continue;
-			CC.insert(num);
-			for (int a : graph[num]) {
-				Component.push(a);
+	total += B;
+	while (max * N * M > total) {
+		max--;
+	}
+	int total_time = INT_MAX;
+	int now_time = 0;
+	while (true) {
+		for (int i = 0; i < N * M; ++i) {
+			if (arr[i] < max) {
+				now_time += max - arr[i];
+			}
+			else if (arr[i] > max) {
+				now_time += 2 * ( arr[i]-max);
 			}
 		}
+		if (now_time < total_time)
+			total_time = now_time;
+		else {
+			max++;
+			break;
+		}
+		max--;
+		now_time = 0;
 	}
-	cout << result;
-	
+	cout << total_time << " " << max;
 }
