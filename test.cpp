12dcc873994859920851854670867fe26c084df5
Lee Jong Hyeok <gurpaper@naver.com>
2024-08-07 11:28:35 +0900

    1389번 케빈 베이컨의 6단계 법칙

--- test.cpp
+++ test.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
-#include <cmath>
+#include <queue>
+#include <vector>
+#include <set>
+#include <map>
 using namespace std;
+
 
 int main()
 {
@@ -8,23 +12,51 @@ int main()
 	std::cin.tie(NULL);
 	std::cout.tie(NULL);
 	
-	int N, r, c;
-	cin >> N >> r >> c;
+	int N, M;
+	cin >> N >> M;
+	map<int,vector<int>> v;
+	int friend1, friend2;
+	for (int i = 0; i < M; ++i) {
+		cin >> friend1 >> friend2;
+		v[friend1].emplace_back(friend2);
+		v[friend2].emplace_back(friend1);
+	}
 
+	vector<int> kevin(N);
+	vector<set<int>> went(N, set<int>());
+	queue<int> q;
+	vector<int> person_count(N+1);
+	int now = 0;
+	int k = 0;
+	for (int i = 0; i < N; ++i) {
+		q.push(i+1);
+		k = 0;
+		person_count[0] = 1;
+		while (!q.empty()) {
+			if (person_count[k] == 0) {
+				k++;
+			}
+			now = q.front();
+			q.pop();
+			person_count[k]--;
+			if (went[i].find(now) != went[i].end()) {
+				continue;
+			}
+			went[i].insert(now);
+			for (int person : v[now]) {
+				person_count[k+1] += 1;
+				q.push(person);
+			}
+			kevin[i] += k;
+		}
+	}
+	int min = 5001;
 	int answer = 0;
-	int board;
-
-	int newR;
-	int newC;
-
-	while (N != 0) {
-		board = pow(2, N - 1);
-		newR = r / board;
-		newC = c / board;
-		r  %= board;
-		c %= board;
-		answer += board * board * (newR * 2 + newC);
-		N -=1;
+	for (int i = 0; i < N; ++i ) {
+		if (min > kevin[i]) {
+			min = kevin[i];
+			answer = i+1;
+		}
 	}
 	cout << answer;
 }
