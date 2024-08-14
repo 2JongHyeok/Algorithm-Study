f08eabc6f95b7b91d83d2c7a200e15c2555b954b
Lee Jong Hyeok <gurpaper@naver.com>
2024-08-14 16:22:26 +0900

    15558번 점프 게임

--- test.cpp
+++ test.cpp
@@ -1,22 +1,8 @@
 #include <iostream>
+#include <queue>
+#include <algorithm>
 
 using namespace std;
-
-long long gcd(long long  a, long long b) {
-	int c;
-	while (b != 0)
-	{
-		c = a % b;
-		a = b;
-		b = c;
-	}
-	return a;
-}
-
-long long lcm(long long a, long long b)
-{
-	return a * b / gcd(a, b);
-}
 
 int main() {
 
@@ -24,36 +10,58 @@ int main() {
     std::cin.tie(NULL);
     std::cout.tie(NULL);
 
-	int T;
-	cin >> T;
-	for (int test = 0; test < T; ++test) {
-		int M, N, x, y;
-		cin >> M >> N >> x >> y;
+    int N, k;
+    cin >> N >> k;
+    string line[2];
+    cin >> line[0] >> line[1];
 
-		int max_x = lcm(M, N) / M;
-		int max_y = lcm(M, N) / N;
-		x--;
-		y--;
+    int step = 0;
+    queue<pair<int,int>> q;
+    vector<int> v(2,0);
+
+    bool clear = false;
+
+    q.push(make_pair(0, 0));
+    v[0] = 1;
+    while (!q.empty()) {
+        pair<int, int> p = q.front();
+        q.pop();
+        if (v[step] == 0) {
+            line[0][step] = '0';
+            line[1][step] = '0';
+            step++;
+            if (v.size() == step+1) {
+                v.emplace_back(0);
+            }
+        }
+        v[step]--;
+        if (line[p.first][p.second] == '0')
+            continue;
+        line[p.first][p.second] = '0';
+
+        if (p.second + k > N - 1) {
+            clear = true;
+            break;
+        }
+        if (line[p.first][max((p.second - 1), 0)] == '1') {
+            q.push(make_pair(p.first, max((p.second - 1), 0)));
+            v[step + 1]++;
+        }
+        if (line[p.first][p.second+1] == '1') {
+            q.push(make_pair(p.first, min((p.second + 1), N-1)));
+            v[step + 1]++;
+        }
+        if (line[((p.first + 1)% 2)][p.second + k] == '1') {
+            q.push(make_pair((p.first + 1) % 2, p.second + k));
+            v[step + 1]++;
+        }
+    }
+    if (clear) {
+        cout << "1";
+    }
+    else {
+        cout << "0";
+    }
 
 
-		int result = -1;
-
-		if (M > N) {
-			for (int i = 0; i < max_x; ++i) {
-				if ((M * i + x) % N == y) {
-					result = M * i + x;
-					break;
-				}
-			}
-		}
-		else {
-			for (int i = 0; i < max_y; ++i) {
-				if ((N * i + y) % M == x) {
-					result = N * i + y;
-					break;
-				}
-			}
-		}
-		cout << result+1<<"\n";
-	}
 }
