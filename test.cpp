b56ae0df3fb43a58f901d2ba7980aa4eff35edea
Lee Jong Hyeok <gurpaper@naver.com>
2024-08-14 16:37:11 +0900

    11286번 절댓값 힙

--- test.cpp
+++ test.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
-#include <queue>
-#include <algorithm>
+#include <set>
 
 using namespace std;
 
@@ -10,58 +9,35 @@ int main() {
     std::cin.tie(NULL);
     std::cout.tie(NULL);
 
-    int N, k;
-    cin >> N >> k;
-    string line[2];
-    cin >> line[0] >> line[1];
+    multiset<pair<int, int>> ms;
 
-    int step = 0;
-    queue<pair<int,int>> q;
-    vector<int> v(2,0);
+    int N;
+    cin >> N;
 
-    bool clear = false;
+    int num;
+    for (int i = 0; i < N; ++i) {
+        cin >> num;
+        if (num == 0) {
+            if (ms.size() == 0) {
+                cout << "0\n";
+                continue;
+            }
+            if ((*ms.begin()).second == 0) {
+                cout <<"-"<< (*ms.begin()).first<<"\n";
+            }
+            else
+                cout << (*ms.begin()).first<<"\n";
+            ms.erase(ms.begin());
+        }
+        else {
+            if (num < 0) {
+                ms.insert(make_pair(num*-1, 0));
 
-    q.push(make_pair(0, 0));
-    v[0] = 1;
-    while (!q.empty()) {
-        pair<int, int> p = q.front();
-        q.pop();
-        if (v[step] == 0) {
-            line[0][step] = '0';
-            line[1][step] = '0';
-            step++;
-            if (v.size() == step+1) {
-                v.emplace_back(0);
+            }
+            else {
+                ms.insert(make_pair(num, 1));
             }
         }
-        v[step]--;
-        if (line[p.first][p.second] == '0')
-            continue;
-        line[p.first][p.second] = '0';
-
-        if (p.second + k > N - 1) {
-            clear = true;
-            break;
-        }
-        if (line[p.first][max((p.second - 1), 0)] == '1') {
-            q.push(make_pair(p.first, max((p.second - 1), 0)));
-            v[step + 1]++;
-        }
-        if (line[p.first][p.second+1] == '1') {
-            q.push(make_pair(p.first, min((p.second + 1), N-1)));
-            v[step + 1]++;
-        }
-        if (line[((p.first + 1)% 2)][p.second + k] == '1') {
-            q.push(make_pair((p.first + 1) % 2, p.second + k));
-            v[step + 1]++;
-        }
     }
-    if (clear) {
-        cout << "1";
-    }
-    else {
-        cout << "0";
-    }
-
 
 }
