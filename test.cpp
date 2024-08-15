800ae710a8740af7cbbd888ff1cd74bba82b46a8
2JongHyeok <gurpaper@naver.com>
2024-08-16 00:59:13 +0900

    15649번 N과 M (1)

--- test.cpp
+++ test.cpp
@@ -2,6 +2,32 @@
 #include <vector>
 
 using namespace std;
+int N, M;
+
+vector<int> visited(9,0);
+vector<int> v(9,0);
+
+void back_tracking(int n) {
+    
+    if (n == M) {
+        for (int num : v) {
+            if (num == 0)
+                break;
+            cout << num << " ";
+        }
+        cout << "\n";
+        return;
+    }
+
+    for (int i = 1; i <= N; ++i) {
+        if (visited[i] == 1)
+            continue;
+        visited[i] = 1;
+        v[n] = i;
+        back_tracking(n + 1);
+        visited[i] = 0;
+    }
+}
 
 int main() {
 
@@ -9,42 +35,6 @@ int main() {
     std::cin.tie(NULL);
     std::cout.tie(NULL);
 
-    int H, W;
-    cin >> H >> W;
-
-    vector<int> v;
-    int block;
-    int maxH = 0;
-    int midW = 0;
-    for (int i = 0; i < W; ++i) {
-        cin >> block;
-        v.emplace_back(block);
-        if (maxH < block) {
-            maxH = block;
-            midW = i;
-        }
-    }
-
-    int firstH = v[0];
-    int result = 0;
-    for (int i = 0; i < midW; ++i) {
-        if (firstH < v[i]) {
-            firstH = v[i];
-        }
-        else {
-            result += firstH - v[i];
-        }
-    }
-    firstH = v[W - 1];
-    for (int i = W - 1; i > midW; --i) {
-        if (firstH < v[i]) {
-            firstH = v[i];
-        }
-        else {
-            result += firstH - v[i];
-        }
-    }
-
-    cout << result;
-
+    cin >> N >> M;
+    back_tracking(0);
 }
