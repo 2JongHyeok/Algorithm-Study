6c0469c230af38121d1993cdb60a9e00c307b800
2JongHyeok <gurpaper@naver.com>
2024-08-15 23:01:08 +0900

    1662번 압축

--- test.cpp
+++ test.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
-#include <vector>
-#include <algorithm>
-#include <queue>
+#include <string>
+#include <stack>
 
 using namespace std;
 
@@ -11,69 +10,52 @@ int main() {
     std::cin.tie(NULL);
     std::cout.tie(NULL);
 
-    vector<pair<int,int>> v;
-
-    int N;
-    int L, H;
-    int midH = 0;
-    int mid = 0;
-    cin >> N;
-    for (int i = 0; i < N; ++i) {
-        cin >> L >> H;
-        v.emplace_back(make_pair(L, H));
-    }
-    sort(v.begin(), v.end());
-    for (int i = 0; i < v.size(); ++i) {
-        if (midH < v[i].second) {
-            midH = v[i].second;
-            mid = i;
+    string s;
+    cin >> s;
+    stack<int> st;
+    int now = 0;
+    for (int i = s.size() - 1; i >= 0; --i) {
+        if (s[i] == ')') {
+            st.push(-1);
+        }
+        else if (s[i] == '(') {
+            now = st.top();
+            if (now == -1) {
+                st.pop();
+                --i;
+                continue;
+            }
+            st.pop();
+            st.pop();
+            now *= int(s[--i]-'0');
+            if (!st.empty()) {
+                if (st.top() != -1) {
+                    int adds = st.top();
+                    st.pop();
+                    st.push(now+adds);
+                    continue;
+                }
+            }
+            st.push(now);
+        }
+        else {
+            if (st.empty()) {
+                st.push(1);
+                continue;
+            }
+            if (st.top() == -1)
+                st.push(1);
+            else {
+                int temp = st.top();
+                st.pop();
+                st.push(temp + 1);
+            }
         }
     }
-    queue<pair<int, int>> q1;
-    queue<pair<int, int>> q2;
-    bool half = false;
-    int s1H = 0;
-    int s2H = 0;
-
-    for (int i = 0; i <= mid; ++i) {
-        if (s1H <= v[i].second) {
-            s1H = v[i].second;
-            q1.push(make_pair(v[i].first, v[i].second));
-        }
+    if (st.empty()) {
+        cout << "0";
     }
-    for (int i = N - 1; i >= mid; --i) {
-        if (s2H <= v[i].second) {
-            s2H = v[i].second;
-            q2.push(make_pair(v[i].first, v[i].second));
-        }
+    else {
+        cout << st.top();
     }
-    int prevL = 0;
-    int prevH = 0;
-    int nowL = 0;
-    int nowH = 0;
-
-    int answer = 0;
-
-    while (!q1.empty()) {
-        prevL = nowL;
-        prevH = nowH;
-        nowL = q1.front().first;
-        nowH= q1.front().second;
-        answer += (nowL - prevL) * prevH;
-        q1.pop();
-    }
-    prevL = 0;
-    prevH = 0;
-    nowL = 0;
-    nowH = 0;
-    while (!q2.empty()) {
-        prevL = nowL;
-        prevH = nowH;
-        nowL = q2.front().first;
-        nowH = q2.front().second;
-        answer += (prevL - nowL) * prevH;
-        q2.pop();
-    }
-    answer += midH;
-    cout << answer;
 }
