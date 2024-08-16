ad704f33d631cc3e354d2b03aec5950d363b5f43
2JongHyeok <gurpaper@naver.com>
2024-08-16 21:51:56 +0900

    15654번 N과 M(5)

--- test.cpp
+++ test.cpp
@@ -1,36 +1,46 @@
 #include <iostream>
 #include <vector>
-#include <set>
+#include <algorithm>
+#include <limits.h>
 
 using namespace std;
 int N, M;
 
 vector<int> v(9, 0);
+vector<int> visited(9,0);
+vector<int> numbers(9, INT_MAX);
 
-void back_tracking(int n, int count) {
+void back_tracking(int n) {
 
-    if (n == M) {
-        for (int num : v) {
-            if (num == 0)
-                break;
-            cout << num << " ";
-        }
-        cout << "\n";
-        return;
-    }
+	if (n == M) {
+		for (int num : v) {
+			if (num == 0)
+				break;
+			cout << num << " ";
+		}
+		cout << "\n";
+		return;
+	}
 
-    for (int i = count; i <= N; ++i) {
-        v[n] = i;
-        back_tracking(n + 1, i);
-    }
+	for (int i = 0; i < N; ++i) {
+		if (visited[i] == 1)continue;
+		v[n] = numbers[i];
+		visited[i] = 1;
+		back_tracking(n + 1);
+		visited[i] = 0;
+	}
 }
 
 int main() {
 
-    std::ios::sync_with_stdio(false);
-    std::cin.tie(NULL);
-    std::cout.tie(NULL);
+	std::ios::sync_with_stdio(false);
+	std::cin.tie(NULL);
+	std::cout.tie(NULL);
 
-    cin >> N >> M;
-    back_tracking(0, 1);
+	cin >> N >> M;
+	for (int i = 0; i < N; ++i) {
+		cin >> numbers[i];
+	}
+	sort(numbers.begin(), numbers.end());
+	back_tracking(0);
 }
