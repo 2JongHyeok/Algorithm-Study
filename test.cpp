02981967b93e781326c8a8141173b177e7131994
Lee Jong Hyeok <gurpaper@naver.com>
2024-08-16 16:08:25 +0900

    15650번 N과 M (2)

--- test.cpp
+++ test.cpp
@@ -1,80 +1,41 @@
 #include <iostream>
 #include <vector>
-#include <queue>
+#include <set>
 
 using namespace std;
+int N, M;
 
-int W[4] = { 0,0,1,-1 };
-int H[4] = { 1,-1,0,0 };
+vector<int> visited(9, 0);
+vector<int> v(9, 0);
+set<int>s;
+void back_tracking(int n, int cnt) {
+
+    if (n == M) {
+        for (int num : v) {
+            if (num == 0)
+                break;
+            cout << num << " ";
+        }
+        cout << "\n";
+        return;
+    }
+
+    for (int i = cnt; i <= N; ++i) {
+        if (visited[i] == 1)
+            continue;
+        visited[i] = 1;
+        v[n] = i;
+        back_tracking(n + 1, i+1);
+        visited[i] = 0;
+    }
+}
 
 int main() {
 
-	std::ios::sync_with_stdio(false);
-	std::cin.tie(NULL);
-	std::cout.tie(NULL);
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(NULL);
+    std::cout.tie(NULL);
 
-	int N,M;
-	cin >> N >> M;
-
-	vector<vector<int>> maze(N, vector<int>(M, 0));
-	queue<pair<pair<int,int>,int>> q;
-
-	string s;
-	int num = 0;
-	int p_x;
-	int p_y;
-	for (int i = 0; i < N; ++i) {
-		cin >> s;
-		for (int j = 0; j < M; ++j) {
-			if (s[j] == '#')
-				num = -1;
-			else if (s[j] == '.')
-				num = 0;
-			else if (s[j] == 'J') {
-				num = 0;
-				p_x = j;
-				p_y = i;
-			}
-			else if (s[j] == 'F') {
-				num = 0;
-				q.push(make_pair(make_pair(i, j), -2));
-			}	
-			maze[i][j] = num;
-		}
-	}
-	q.push(make_pair(make_pair(p_y, p_x), 1));
-	bool escape = false;
-	num = 0;
-
-	while (!q.empty()) {
-		pair<int, int> p = q.front().first;
-		int who = q.front().second;
-		q.pop();
-		if (maze[p.first][p.second] != 0)continue;
-		maze[p.first][p.second] = who;
-		for (int k = 0; k < 4; ++k) {
-			int x = p.second + W[k];
-			int y = p.first + H[k];
-			if (x < 0 || x >= M || y < 0 || y >= N) {
-				if (who >= 1) {
-					escape = true;
-					num = who;
-					break;
-				}
-				continue;
-			}
-			if (maze[y][x] != 0) continue;
-			if (who == -2) {
-				q.push(make_pair(make_pair(y, x), -2));
-			}
-			else
-				q.push(make_pair(make_pair(y, x),who+1));
-		}
-		if (escape)
-			break;
-	}
-	if (escape)
-		cout << num;
-	else
-		cout << "IMPOSSIBLE";
+    cin >> N >> M;
+    back_tracking(0,1);
 }
