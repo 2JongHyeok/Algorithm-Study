bdc1c75826da3e1e886e4231c9902bb6eea70096
Lee Jong Hyeok <gurpaper@naver.com>
2024-08-16 10:50:41 +0900

    1926번 그림

--- test.cpp
+++ test.cpp
@@ -1,33 +1,11 @@
 #include <iostream>
 #include <vector>
+#include <queue>
 
 using namespace std;
-int N, M;
 
-vector<int> visited(9,0);
-vector<int> v(9,0);
-
-void back_tracking(int n) {
-    
-    if (n == M) {
-        for (int num : v) {
-            if (num == 0)
-                break;
-            cout << num << " ";
-        }
-        cout << "\n";
-        return;
-    }
-
-    for (int i = 1; i <= N; ++i) {
-        if (visited[i] == 1)
-            continue;
-        visited[i] = 1;
-        v[n] = i;
-        back_tracking(n + 1);
-        visited[i] = 0;
-    }
-}
+int W[4] = { 0,0,1,-1 };
+int H[4] = { 1,-1,0,0 };
 
 int main() {
 
@@ -35,6 +13,52 @@ int main() {
     std::cin.tie(NULL);
     std::cout.tie(NULL);
 
-    cin >> N >> M;
-    back_tracking(0);
+    int n, m;
+    cin >> n >> m;
+
+    vector<vector<int>> paper(n, vector<int>(m, 0));
+
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < m; ++j) {
+            cin >> paper[i][j];
+        }
+    }
+
+    queue<pair<int,int>> q;
+
+    vector<vector<bool>> visited(n, vector<bool>(m, false));
+
+    int count = 0;
+    int maxW = 0;
+    int nowW = 0;
+
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < m; ++j) {
+            if (paper[i][j] == 0) continue;
+            if (visited[i][j]) continue;
+            q.push(make_pair(i, j));
+            nowW = 0;
+            count++;
+            while (!q.empty()) {
+                pair<int, int> p = q.front();
+                q.pop();
+                if (visited[p.first][p.second]) continue;
+                visited[p.first][p.second] = true;
+                nowW++;
+                for (int k = 0; k < 4; ++k) {
+                    if (p.first + H[k] < 0) continue;
+                    if (p.first + H[k] >= n) continue;
+                    if (p.second + W[k] <0) continue;
+                    if (p.second + W[k] >= m) continue;
+                    if (visited[p.first + H[k]][p.second + W[k]]) continue;
+                    if (paper[p.first + H[k]][p.second + W[k]] == 0)continue;
+                    q.push(make_pair(p.first + H[k], p.second + W[k]));
+                }
+            }
+            if (maxW < nowW)
+                maxW = nowW;
+        }
+    }
+
+    cout << count << "\n" << maxW;
 }
