6104b810cab758a87ac51c0ba6297b6b3948b5c5
Lee Jong Hyeok <gurpaper@naver.com>
2024-08-16 14:52:52 +0900

    4179번 불!

--- test.cpp
+++ test.cpp
@@ -9,50 +9,72 @@ int H[4] = { 1,-1,0,0 };
 
 int main() {
 
-    std::ios::sync_with_stdio(false);
-    std::cin.tie(NULL);
-    std::cout.tie(NULL);
+	std::ios::sync_with_stdio(false);
+	std::cin.tie(NULL);
+	std::cout.tie(NULL);
 
-    int M,N;
-    cin >> M >> N;
+	int N,M;
+	cin >> N >> M;
 
-    vector<vector<int>> box(N, vector<int>(M, 0));
-    queue<pair<pair<int,int>,int>> q;
-    vector<vector<bool>> ripe(N, vector<bool>(M, false));
+	vector<vector<int>> maze(N, vector<int>(M, 0));
+	queue<pair<pair<int,int>,int>> q;
 
-    for (int i = 0; i < N; ++i) {
-        for (int j = 0; j < M; ++j) {
-            cin >> box[i][j];
-            if(box[i][j] == 1)
-                q.push(make_pair(make_pair(i, j), 0));
-        }
-    }
-    int max=0;
-    while (!q.empty()) {
-        pair<int, int> p = q.front().first;
-        int days = q.front().second;
-        q.pop();
-        if (ripe[p.first][p.second]) continue;
-        ripe[p.first][p.second] = true;
-        box[p.first][p.second] = 1;
-        for (int k = 0; k < 4; ++k) {
-            int x = p.second + W[k];
-            int y = p.first + H[k];
-            if (x < 0 || x >= M || y < 0 || y >= N) continue;
-            if (ripe[y][x]) continue;
-            if (box[y][x] == -1)continue;
-            if (box[y][x] == 1)continue;
-            q.push(make_pair(make_pair(y, x),days+1));
-        }
-        max = days;
-    }
-    for (auto a : box) {
-        for (auto b : a) {
-            if (b == 0) {
-                cout << "-1";
-                return 0;
-            }
-        }
-    }
-    cout << max;
+	string s;
+	int num = 0;
+	int p_x;
+	int p_y;
+	for (int i = 0; i < N; ++i) {
+		cin >> s;
+		for (int j = 0; j < M; ++j) {
+			if (s[j] == '#')
+				num = -1;
+			else if (s[j] == '.')
+				num = 0;
+			else if (s[j] == 'J') {
+				num = 0;
+				p_x = j;
+				p_y = i;
+			}
+			else if (s[j] == 'F') {
+				num = 0;
+				q.push(make_pair(make_pair(i, j), -2));
+			}	
+			maze[i][j] = num;
+		}
+	}
+	q.push(make_pair(make_pair(p_y, p_x), 1));
+	bool escape = false;
+	num = 0;
+
+	while (!q.empty()) {
+		pair<int, int> p = q.front().first;
+		int who = q.front().second;
+		q.pop();
+		if (maze[p.first][p.second] != 0)continue;
+		maze[p.first][p.second] = who;
+		for (int k = 0; k < 4; ++k) {
+			int x = p.second + W[k];
+			int y = p.first + H[k];
+			if (x < 0 || x >= M || y < 0 || y >= N) {
+				if (who >= 1) {
+					escape = true;
+					num = who;
+					break;
+				}
+				continue;
+			}
+			if (maze[y][x] != 0) continue;
+			if (who == -2) {
+				q.push(make_pair(make_pair(y, x), -2));
+			}
+			else
+				q.push(make_pair(make_pair(y, x),who+1));
+		}
+		if (escape)
+			break;
+	}
+	if (escape)
+		cout << num;
+	else
+		cout << "IMPOSSIBLE";
 }
