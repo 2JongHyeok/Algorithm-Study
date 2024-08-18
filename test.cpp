a75dce2e4c334824bd46fdb9005a29bbd98408e8
2JongHyeok <gurpaper@naver.com>
2024-08-18 22:16:56 +0900

    11403번 경로 찾기

--- test.cpp
+++ test.cpp
@@ -1,27 +1,18 @@
 #include <iostream>
-#include <set>
 #include <vector>
+#include <set>
 
 using namespace std;
+vector<vector<int>> visited(100, vector<int>(100, 0));
+vector<set<int>> can_go(100);
 
-int N, M;
-vector<int> v(9, 0);
-set<int> numbers;
 
-void back_tracking(int n) {
-	if (n == M) {
-		for (int i = 0; i < n; ++i) {
-			cout << v[i] << " ";
-		}
-		cout << "\n";
-		return;
-	}
-	for (int num : numbers) {
-		v[n] = num;
-		if (n > 0) {
-			if (v[n - 1] > v[n]) continue;
-		}
-		back_tracking(n + 1);
+void search(int start, int end) {
+	visited[start][end] = 1;
+	for (int num : can_go[end]) {
+		if (visited[start][num] == 1) continue;
+		visited[start][num] = 1;
+		search(start, num);
 	}
 }
 
@@ -31,12 +22,28 @@ int main() {
 	std::cin.tie(NULL);
 	std::cout.tie(NULL);
 
-	cin >> N >> M;
+	int N;
+	cin >> N;
+	
 	int num;
 	for (int i = 0; i < N; ++i) {
-		cin >> num;
-		numbers.insert(num);
+		for (int j = 0; j < N; ++j) {
+			cin >> num;
+			if (num == 1)
+				can_go[i].insert(j);
+		}
+	}
+	for (int i = 0; i < N; ++i) {
+		for (int num : can_go[i]) {
+			search(i, num);
+		}
 	}
 
-	back_tracking(0);
+	for (int i = 0; i < N; ++i) {
+		for (int j = 0; j < N; ++j) {
+			cout << visited[i][j] << " ";
+		}
+		cout << "\n";
+	}
+
 }
