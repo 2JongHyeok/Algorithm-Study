44452ff0aa0d7574a425b03f8bc2736dbd98a8d1
Lee Jong Hyeok <gurpaper@naver.com>
2024-08-19 14:27:39 +0900

    10866번 덱

--- test.cpp
+++ test.cpp
@@ -1,20 +1,13 @@
 #include <iostream>
-#include <vector>
-#include <set>
+#include <string>
 
 using namespace std;
-vector<vector<int>> visited(100, vector<int>(100, 0));
-vector<set<int>> can_go(100);
 
-
-void search(int start, int end) {
-	visited[start][end] = 1;
-	for (int num : can_go[end]) {
-		if (visited[start][num] == 1) continue;
-		visited[start][num] = 1;
-		search(start, num);
-	}
-}
+struct Node {
+	Node* next;
+	Node* prev;
+	int num;
+};
 
 int main() {
 
@@ -22,28 +15,98 @@ int main() {
 	std::cin.tie(NULL);
 	std::cout.tie(NULL);
 
+	int total = 0;
+	Node* front = nullptr;
+	Node* back = nullptr;
+
 	int N;
 	cin >> N;
-	
+	string s;
 	int num;
 	for (int i = 0; i < N; ++i) {
-		for (int j = 0; j < N; ++j) {
+		cin >> s;
+		if (s == "push_front") {
 			cin >> num;
-			if (num == 1)
-				can_go[i].insert(j);
+			Node* new_node = new Node;
+			new_node->num = num;
+			if (total == 0) {
+				new_node->next = nullptr;
+				front = new_node;
+				back = new_node;
+			}
+			else {
+				front->prev = new_node;
+				new_node->next = front;
+				front = new_node;
+			}
+			total++;
+		}
+		else if (s == "push_back") {
+			cin >> num;
+			Node* new_node = new Node;
+			new_node->num = num;
+			if (total == 0) {
+				new_node->next = nullptr;
+				front = new_node;
+				back = new_node;
+			}
+			else {
+				back->next = new_node;
+				new_node->prev = back;
+				back = new_node;
+			}
+			total++;
+		}
+		else if (s == "pop_front") {
+			if (total == 0) {
+				cout << "-1\n";
+			}
+			else {
+				cout << front->num << "\n";
+				Node* now_node = front;
+				front = front->next;
+				delete now_node;
+				total--;
+			}
+		}
+		else if (s == "pop_back") {
+			if (total == 0) {
+				cout << "-1\n";
+			}
+			else {
+				cout << back->num << "\n";
+				Node* now_node = back;
+				back = back->prev;
+				delete now_node;
+				total--;
+			}
+		}
+		else if (s == "size") {
+			cout << total << "\n";
+		}
+		else if (s == "empty") {
+			if (total == 0) {
+				cout << "1\n";
+			}
+			else {
+				cout << "0\n";
+			}
+		}
+		else if (s == "front") {
+			if (total == 0) {
+				cout << "-1\n";
+			}
+			else {
+				cout << front->num << "\n";
+			}
+		}
+		else if (s == "back") {
+			if (total == 0) {
+				cout << "-1\n";
+			}
+			else {
+				cout << back->num << "\n";
+			}
 		}
 	}
-	for (int i = 0; i < N; ++i) {
-		for (int num : can_go[i]) {
-			search(i, num);
-		}
-	}
-
-	for (int i = 0; i < N; ++i) {
-		for (int j = 0; j < N; ++j) {
-			cout << visited[i][j] << " ";
-		}
-		cout << "\n";
-	}
-
-}
+}	
