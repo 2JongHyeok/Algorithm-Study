38e709ba148b0683805582bc06f17e32bce44224
Lee Jong Hyeok <gurpaper@naver.com>
2024-08-20 17:05:55 +0700

    14940번 쉬운 최단거리

--- test.cpp
+++ test.cpp
@@ -1,13 +1,9 @@
 #include <iostream>
-#include <string>
+#include <vector>
+#include <queue>
+
 
 using namespace std;
-
-struct Node {
-	Node* next;
-	Node* prev;
-	int num;
-};
 
 int main() {
 
@@ -15,98 +11,67 @@ int main() {
 	std::cin.tie(NULL);
 	std::cout.tie(NULL);
 
-	int total = 0;
-	Node* front = nullptr;
-	Node* back = nullptr;
 
-	int N;
-	cin >> N;
-	string s;
-	int num;
-	for (int i = 0; i < N; ++i) {
-		cin >> s;
-		if (s == "push_front") {
-			cin >> num;
-			Node* new_node = new Node;
-			new_node->num = num;
-			if (total == 0) {
-				new_node->next = nullptr;
-				front = new_node;
-				back = new_node;
-			}
-			else {
-				front->prev = new_node;
-				new_node->next = front;
-				front = new_node;
-			}
-			total++;
-		}
-		else if (s == "push_back") {
-			cin >> num;
-			Node* new_node = new Node;
-			new_node->num = num;
-			if (total == 0) {
-				new_node->next = nullptr;
-				front = new_node;
-				back = new_node;
-			}
-			else {
-				back->next = new_node;
-				new_node->prev = back;
-				back = new_node;
-			}
-			total++;
-		}
-		else if (s == "pop_front") {
-			if (total == 0) {
-				cout << "-1\n";
-			}
-			else {
-				cout << front->num << "\n";
-				Node* now_node = front;
-				front = front->next;
-				delete now_node;
-				total--;
+	int n, m;
+	cin >> n >> m;
+	vector<vector<int>> v(n, vector<int>(m, 0));
+
+	int dest_x;
+	int dest_y;
+
+	for (int i = 0; i < n; ++i) {
+		for (int j = 0; j < m; ++j) {
+			cin >> v[i][j];
+			if (v[i][j] == 2) {
+				dest_x = j;
+				dest_y = i;
+				v[i][j] = 0;
 			}
 		}
-		else if (s == "pop_back") {
-			if (total == 0) {
-				cout << "-1\n";
-			}
-			else {
-				cout << back->num << "\n";
-				Node* now_node = back;
-				back = back->prev;
-				delete now_node;
-				total--;
-			}
+	}
+
+	queue<pair<pair<int, int>,int>> q;
+	q.push(make_pair(make_pair(dest_y, dest_x),0));
+
+	int x, y, count;
+	int dx[4] = { 0,0,1,-1 };
+	int dy[4] = { 1,-1,0,0 };
+	while (!q.empty()) {
+		y = q.front().first.first;
+		x = q.front().first.second;
+		count = q.front().second;
+		q.pop();
+		if (v[y][x] > 1) continue;
+		for (int i = 0; i < 4; ++i) {
+			int new_x = x + dx[i];
+			int new_y = y + dy[i];
+			if (new_x < 0 || new_x >= m || new_y < 0 || new_y >= n) continue;
+			if (v[new_y][new_x] > 1)continue;
+			if (v[new_y][new_x] == 0)continue;
+			q.push(make_pair(make_pair(new_y, new_x), count+1));
 		}
-		else if (s == "size") {
-			cout << total << "\n";
+		v[y][x] = count;
+	}
+	for (int i = 0; i < n; ++i) {
+		for (int j = 0; j < m; ++j) {
+			if (v[i][j] == 1)
+				v[i][j] = -1;
 		}
-		else if (s == "empty") {
-			if (total == 0) {
-				cout << "1\n";
-			}
-			else {
-				cout << "0\n";
-			}
+	}
+	for (int i = 0; i < 4; ++i) {
+		int new_x = dest_x + dx[i];
+		int new_y = dest_y + dy[i];
+		if (new_x < 0 || new_x >= m || new_y < 0 || new_y >= n) continue;
+		if (v[new_y][new_x] == 0)continue;
+		if (v[new_y][new_x] == -1)continue;
+		v[new_y][new_x] = 1;
+	}
+	
+
+	for (int i = 0; i < n; ++i) {
+		for (int j = 0; j < m; ++j) {
+			cout << v[i][j] << " ";
 		}
-		else if (s == "front") {
-			if (total == 0) {
-				cout << "-1\n";
-			}
-			else {
-				cout << front->num << "\n";
-			}
-		}
-		else if (s == "back") {
-			if (total == 0) {
-				cout << "-1\n";
-			}
-			else {
-				cout << back->num << "\n";
-			}
-		}
+		cout << "\n";
 	}
 }	
