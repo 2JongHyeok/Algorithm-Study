cd234ec2480d9352ce6fcbada0dc0d6b5f672c61
Lee Jong Hyeok <gurpaper@naver.com>
2024-08-21 17:32:00 +0700

    5430번 AC

--- test.cpp
+++ test.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
-#include <vector>
-#include <queue>
-
+#include <deque>
+#include <ctype.h>
 
 using namespace std;
 
@@ -11,67 +10,68 @@ int main() {
 	std::cin.tie(NULL);
 	std::cout.tie(NULL);
 
-
-	int n, m;
-	cin >> n >> m;
-	vector<vector<int>> v(n, vector<int>(m, 0));
-
-	int dest_x;
-	int dest_y;
-
-	for (int i = 0; i < n; ++i) {
-		for (int j = 0; j < m; ++j) {
-			cin >> v[i][j];
-			if (v[i][j] == 2) {
-				dest_x = j;
-				dest_y = i;
-				v[i][j] = 0;
+	int T;
+	cin >> T;
+	for (int i = 0; i < T; ++i) {
+		string p;
+		cin >> p;
+		int n;
+		cin >> n;
+		deque<int> d;
+		string numbers;
+		cin >> numbers;
+		int number=0;
+		for (char c : numbers) {
+			if (isdigit(c)) {
+				number *= 10;
+				number += int(c-'0');
+			}
+			if (c == ','||c == ']') {
+				d.push_back(number);
+				number = 0;
 			}
 		}
-	}
-
-	queue<pair<pair<int, int>,int>> q;
-	q.push(make_pair(make_pair(dest_y, dest_x),0));
-
-	int x, y, count;
-	int dx[4] = { 0,0,1,-1 };
-	int dy[4] = { 1,-1,0,0 };
-	while (!q.empty()) {
-		y = q.front().first.first;
-		x = q.front().first.second;
-		count = q.front().second;
-		q.pop();
-		if (v[y][x] > 1) continue;
-		for (int i = 0; i < 4; ++i) {
-			int new_x = x + dx[i];
-			int new_y = y + dy[i];
-			if (new_x < 0 || new_x >= m || new_y < 0 || new_y >= n) continue;
-			if (v[new_y][new_x] > 1)continue;
-			if (v[new_y][new_x] == 0)continue;
-			q.push(make_pair(make_pair(new_y, new_x), count+1));
+		bool rev = false;
+		bool error = false;
+		for (char c : p) {
+			if (c == 'R') {
+				if (rev)rev = false;
+				else rev = true;
+			}
+			if (c == 'D') {
+				if (n == 0) {
+					error = true;
+					break;
+				}
+				else {
+					if (rev) d.pop_back();
+					else d.pop_front();
+					n--;
+				}
+			}
 		}
-		v[y][x] = count;
-	}
-	for (int i = 0; i < n; ++i) {
-		for (int j = 0; j < m; ++j) {
-			if (v[i][j] == 1)
-				v[i][j] = -1;
-		}
-	}
-	for (int i = 0; i < 4; ++i) {
-		int new_x = dest_x + dx[i];
-		int new_y = dest_y + dy[i];
-		if (new_x < 0 || new_x >= m || new_y < 0 || new_y >= n) continue;
-		if (v[new_y][new_x] == 0)continue;
-		if (v[new_y][new_x] == -1)continue;
-		v[new_y][new_x] = 1;
-	}
-	
 
-	for (int i = 0; i < n; ++i) {
-		for (int j = 0; j < m; ++j) {
-			cout << v[i][j] << " ";
+		if (error) cout << "error\n";
+		else {
+			cout << "[";
+			if (n == 0) {
+				cout << "]\n";
+				continue;
+			}
+			if (rev) {
+				for (int i = 0; i < n - 1; ++i) {
+					cout << d.back() << ",";
+					d.pop_back();
+				}
+				cout << d.back() << "]\n";
+			}
+			else {
+				for (int i = 0; i < n - 1; ++i) {
+					cout << d.front() << ",";
+					d.pop_front();
+				}
+				cout << d.front() << "]\n";
+			}
 		}
-		cout << "\n";
 	}
 }	
