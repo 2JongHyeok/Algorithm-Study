382271182f904c5e85874d1de944af2a56711d0b
2JongHyeok <gurpaper@naver.com>
2024-08-28 21:07:42 +0900

    1149번 RGB거리

--- test.cpp
+++ test.cpp
@@ -1,49 +1,29 @@
 #include <iostream>
-#include <queue>
-#include <map>
-
+#include <algorithm>
 
 using namespace std;
-map<long long, bool> visited;
 
 int main() {
 	std::ios::sync_with_stdio(false);
 	std::cin.tie(NULL);
 	std::cout.tie(NULL);
 	
-	int A, B;
-	cin >> A >> B;
-	queue<pair<long long, long long>> q;
-
-	q.push(make_pair(A, 1));
-	visited[A] = true;
-	
-	int answer = -1;
-
-	while (!q.empty()) {
-		long long num = q.front().first;
-		int count = q.front().second;
-		q.pop();
-		if (num * 2 <= 1'000'000'000) {
-			if (num * 2 == B) {
-				answer = count+1;
-				break;
-			}
-			if (!visited[num * 2]) {
-				visited[num * 2] = true;
-				q.push(make_pair(num * 2, count + 1));
-			}
-		}
-		if (num * 10 + 1 <= 1'000'000'000) {
-			if (num * 10+1 == B) {
-				answer = count+1;
-				break;
-			}
-			if (!visited[num * 10 + 1]) {
-				visited[num * 10 + 1] = true;
-				q.push(make_pair(num * 10 + 1, count + 1));
-			}
+	int N;
+	cin >> N;
+	int houses[1000][3]{0};
+	for (int i = 0; i < N; ++i) {
+		for (int j = 0; j < 3; ++j) {
+			cin >> houses[i][j];
 		}
 	}
-	cout << answer;
+	int dp[1000][3]{0};
+	dp[0][0] = houses[0][0];
+	dp[0][1] = houses[0][1];
+	dp[0][2] = houses[0][2];
+	for (int i = 1; i < N; ++i) {
+		dp[i][0] = min(dp[i - 1][1], dp[i - 1][2]) + houses[i][0];
+		dp[i][1] = min(dp[i - 1][0], dp[i - 1][2]) + houses[i][1];
+		dp[i][2] = min(dp[i - 1][1], dp[i - 1][0]) + houses[i][2];
+	}
+	cout << min(min(dp[N - 1][0], dp[N - 1][1]), dp[N - 1][2]);
 }
