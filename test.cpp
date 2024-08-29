be61cdd6f35fb55a7cdba1f81252b038c91663bc
Lee Jong Hyeok <gurpaper@naver.com>
2024-08-29 10:04:49 +0900

    1932번 정수 삼각형

--- test.cpp
+++ test.cpp
@@ -1,7 +1,10 @@
 #include <iostream>
 #include <algorithm>
+#include <vector>
 
 using namespace std;
+
+
 
 int main() {
 	std::ios::sync_with_stdio(false);
@@ -10,20 +13,33 @@ int main() {
 	
 	int N;
 	cin >> N;
-	int houses[1000][3]{0};
+	vector<vector<int>>nums(N,vector<int>(N,0));
+	vector<vector<int>>dp(N,vector<int>(N,0));
 	for (int i = 0; i < N; ++i) {
-		for (int j = 0; j < 3; ++j) {
-			cin >> houses[i][j];
+		for (int j = 0; j < i+1; ++j) {
+			cin >> nums[i][j];
 		}
 	}
-	int dp[1000][3]{0};
-	dp[0][0] = houses[0][0];
-	dp[0][1] = houses[0][1];
-	dp[0][2] = houses[0][2];
+	dp[0][0] = nums[0][0];
 	for (int i = 1; i < N; ++i) {
-		dp[i][0] = min(dp[i - 1][1], dp[i - 1][2]) + houses[i][0];
-		dp[i][1] = min(dp[i - 1][0], dp[i - 1][2]) + houses[i][1];
-		dp[i][2] = min(dp[i - 1][1], dp[i - 1][0]) + houses[i][2];
+		for (int j = 0; j < i+1; ++j) {
+			if (j == 0) {
+				dp[i][j] = dp[i - 1][j] + nums[i][j];
+			}
+			else if (j == i) {
+				dp[i][j] = dp[i - 1][j - 1] + nums[i][j];
+			}
+			else {
+				dp[i][j] = max(dp[i - 1][j], dp[i - 1][j - 1]) + nums[i][j];
+			}
+		}
 	}
-	cout << min(min(dp[N - 1][0], dp[N - 1][1]), dp[N - 1][2]);
+
+	int maxi = 0;
+
+	for (int num : dp[N - 1]) {
+		if (maxi < num)
+			maxi = num;
+	}
+	cout << maxi;
 }
