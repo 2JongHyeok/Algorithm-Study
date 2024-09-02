d2d96918a7179cb9a602cf2580c8378ac340a7c3
Lee Jong Hyeok <gurpaper@naver.com>
2024-09-02 12:03:14 +0900

    1916번 최소비용 구하기

--- test.cpp
+++ test.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <queue>
+#include <cmath>
 
 using namespace std;
-
 
 int main() {
 	std::ios::sync_with_stdio(false);
@@ -12,35 +13,37 @@ int main() {
 	
 	int N, M;
 	cin >> N >> M;
-	int num;
-	int line;
-	vector<vector<int>> dp(N, vector<int>(N, 0));
-	for (int i = 0; i < N; ++i) {
-		line = 0;
-		for (int j = 0; j < N; ++j) {
-			cin >> num;
-			line += num;
-			if (i > 0) {
-				dp[i][j] = dp[i - 1][j] + line;
-			}
-			else
-				dp[i][j] = line;
-		}
-	}
-	int x1, x2, y1, y2;
+	
+	vector<vector<pair<int, int>>> buses(M+1, vector<pair<int,int>>());
+	vector<int>costs(N + 1, INFINITY);
+
+	std::priority_queue<pair<int,long long>, std::vector<pair<int, long long>>, std::greater<pair<int, long long>>> pq;
+
+	int start, end;
+	long long cost;
+
 	for (int i = 0; i < M; ++i) {
-		cin >> y1 >> x1 >> y2 >> x2;
-		x1--; x2--; y1--; y2--;
-		int answer = dp[y2][x2];
-		if (x1 - 1 >= 0) {
-			answer -= dp[y2][x1 - 1];
-		}
-		if (y1 - 1 >= 0) {
-			answer -= dp[y1 - 1][x2];
-		}
-		if(x1-1>=0 && y1-1>=0)
-			 answer+= dp[y1 - 1][x1 - 1];
-		cout << answer << "\n";
+		cin >> start >> end >> cost;
+		buses[start].emplace_back(make_pair(end, cost));
 	}
 
+	cin >> start >> end;
+	pq.push(make_pair(start, 0));
+
+	while (!pq.empty()) {
+		int current = pq.top().first;
+		int value = pq.top().second;
+		pq.pop();
+		if (costs[current] < value) continue;	
+		for (auto b : buses[current]) {
+			int next = b.first;
+			int nextValue = b.second + value;
+			if (nextValue < costs[next]) {
+				costs[next] = nextValue;
+				pq.push(make_pair(next, nextValue));
+			}
+		}
+	}
+
+	cout << costs[end];
 }
