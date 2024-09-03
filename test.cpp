476f5259b84610ed968ad05a8042f47957f4693c
Lee Jong Hyeok <gurpaper@naver.com>
2024-09-03 14:17:30 +0900

    2096번 내려가기

--- test.cpp
+++ test.cpp
@@ -1,49 +1,42 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
-#include <queue>
-#include <cmath>
 
 using namespace std;
-
+int maxDp[2][3];
+int minDp[2][3];
 int main() {
 	std::ios::sync_with_stdio(false);
 	std::cin.tie(NULL);
 	std::cout.tie(NULL);
 	
-	int N, M;
-	cin >> N >> M;
-	
-	vector<vector<pair<int, int>>> buses(1001, vector<pair<int,int>>());
-	vector<int>costs(1001, INFINITY);
-
-	std::priority_queue<pair<int,long long>, std::vector<pair<int, long long>>, std::greater<pair<int, long long>>> pq;
-
-	int start, end;
-	long long cost;
-
-	for (int i = 0; i < M; ++i) {
-		cin >> start >> end >> cost;
-		buses[start].emplace_back(make_pair(end, cost));
+	int N;
+	cin >> N;
+	int nums[3];
+	for (int i = 0; i < 3; ++i) {
+		cin >> nums[i];
 	}
-
-	cin >> start >> end;
-	pq.push(make_pair(start, 0));
-
-	while (!pq.empty()) {
-		int current = pq.top().first;
-		int value = pq.top().second;
-		pq.pop();
-		if (costs[current] < value) continue;	
-		for (auto b : buses[current]) {
-			int next = b.first;
-			int nextValue = b.second + value;
-			if (nextValue < costs[next]) {
-				costs[next] = nextValue;
-				pq.push(make_pair(next, nextValue));
-			}
+	maxDp[0][0] = nums[0];
+	maxDp[0][1] = nums[1];
+	maxDp[0][2] = nums[2];
+	minDp[0][0] = nums[0];
+	minDp[0][1] = nums[1];
+	minDp[0][2] = nums[2];
+	for (int i = 1; i < N; ++i) {
+		for (int j = 0; j < 3; ++j) {
+			cin >> nums[j];
+		}
+		maxDp[1][0] = max(maxDp[0][1], maxDp[0][0]) + nums[0];
+		maxDp[1][1] = max(max(maxDp[0][1], maxDp[0][0]),maxDp[0][2]) + nums[1];
+		maxDp[1][2] = max(maxDp[0][1], maxDp[0][2]) + nums[2];
+		minDp[1][0] = min(minDp[0][1], minDp[0][0]) + nums[0];
+		minDp[1][1] = min(min(minDp[0][1], minDp[0][0]), minDp[0][2]) + nums[1];
+		minDp[1][2] = min(minDp[0][1], minDp[0][2]) + nums[2];
+		for (int i = 0; i < 3; ++i) {
+			maxDp[0][i] = maxDp[1][i];
+			minDp[0][i] = minDp[1][i];
 		}
 	}
-
-	cout << costs[end];
+	cout << max(max(maxDp[0][1], maxDp[0][0]), maxDp[0][2])<<" ";
+	cout << min(min(minDp[0][1], minDp[0][0]), minDp[0][2]);
 }
