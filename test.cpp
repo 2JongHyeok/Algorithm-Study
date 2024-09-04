760929dcf3d305caf0b7f16094908899d46f4db6
Lee Jong Hyeok <gurpaper@naver.com>
2024-09-04 12:12:28 +0900

    9251번 LCS

--- test.cpp
+++ test.cpp
@@ -1,42 +1,30 @@
 #include <iostream>
-#include <vector>
+#include <string>
 #include <algorithm>
 
 using namespace std;
-int maxDp[2][3];
-int minDp[2][3];
+int dp[1001][1001]{};
+
 int main() {
 	std::ios::sync_with_stdio(false);
 	std::cin.tie(NULL);
 	std::cout.tie(NULL);
 	
-	int N;
-	cin >> N;
-	int nums[3];
-	for (int i = 0; i < 3; ++i) {
-		cin >> nums[i];
+	string s1, s2;
+	cin >> s1 >> s2;
+	if (s1 == s2) {
+		cout << s1.size();
 	}
-	maxDp[0][0] = nums[0];
-	maxDp[0][1] = nums[1];
-	maxDp[0][2] = nums[2];
-	minDp[0][0] = nums[0];
-	minDp[0][1] = nums[1];
-	minDp[0][2] = nums[2];
-	for (int i = 1; i < N; ++i) {
-		for (int j = 0; j < 3; ++j) {
-			cin >> nums[j];
-		}
-		maxDp[1][0] = max(maxDp[0][1], maxDp[0][0]) + nums[0];
-		maxDp[1][1] = max(max(maxDp[0][1], maxDp[0][0]),maxDp[0][2]) + nums[1];
-		maxDp[1][2] = max(maxDp[0][1], maxDp[0][2]) + nums[2];
-		minDp[1][0] = min(minDp[0][1], minDp[0][0]) + nums[0];
-		minDp[1][1] = min(min(minDp[0][1], minDp[0][0]), minDp[0][2]) + nums[1];
-		minDp[1][2] = min(minDp[0][1], minDp[0][2]) + nums[2];
-		for (int i = 0; i < 3; ++i) {
-			maxDp[0][i] = maxDp[1][i];
-			minDp[0][i] = minDp[1][i];
+
+	for (int i = 1; i <= s1.size(); ++i) {
+		for (int j = 1; j <= s2.size(); ++j) {
+			if (s1[i-1] == s2[j-1]) {
+				dp[i][j] = dp[i - 1][j - 1] + 1;
+			}
+			else {
+				dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+			}
 		}
 	}
-	cout << max(max(maxDp[0][1], maxDp[0][0]), maxDp[0][2])<<" ";
-	cout << min(min(minDp[0][1], minDp[0][0]), minDp[0][2]);
+	cout << dp[s1.size()][s2.size()];
 }
