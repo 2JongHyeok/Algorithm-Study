35bebb3bec8b21809f79e071ef927a6cf67d774d
Lee Jong Hyeok <gurpaper@naver.com>
2024-09-11 18:46:20 +0900

    1753번 최단경로

--- test.cpp
+++ test.cpp
@@ -1,24 +1,32 @@
 #include <iostream>
 #include <vector>
+#include <queue>
 
 using namespace std;
-vector<int> parent(51,0);
-vector<vector<int>> party(50, vector<int>());
 
-int Find(int num) {
-	if (num != parent[num])
-		parent[num] = Find(parent[num]);
-	return parent[num];
-}
+int INF = 1e9;
 
-void Union(int a, int b) {
-	a = Find(a);
-	b = Find(b);
+vector<vector<pair<int, int>>> v(20'001, vector<pair<int,int>>()) ;
+int val[20'001];
 
-	if (a < b)
-		parent[b] = a;
-	else {
-		parent[a] = b;
+void dijkstra(int start) {
+	val[start] = 0;
+	priority_queue<pair<int, int>, vector<pair<int,int>>, greater<pair<int,int>>> pq;
+	pq.push(make_pair(0, start));
+
+	while (!pq.empty()) {
+		int current = pq.top().second;
+		int distance = pq.top().first;
+		pq.pop();
+		if (val[current] < distance)continue;
+		for (int i = 0; i < v[current].size(); ++i) {
+			int next = v[current][i].second;
+			int nextDis = distance + v[current][i].first;
+			if (nextDis < val[next]) {
+				val[next] = nextDis;
+				pq.push(make_pair(nextDis, next));
+			}
+		}
 	}
 }
 
@@ -27,36 +35,26 @@ int main()
 	std::ios::sync_with_stdio(false);
 	std::cin.tie(NULL);
 	std::cout.tie(NULL);
-	
-	int N, M;
-	cin >> N >> M;
-	for (int i = 0; i <= N; ++i) {
-		parent[i] = i;
-	}
-	int knowTruth;
-	cin >> knowTruth;
-	for (int i = 0; i < knowTruth; ++i) {
-		int num;
-		cin >> num;
-		parent[num] = 0;
+	int V, E;
+	cin >> V >>E;
+	for (int i = 1; i <= V; ++i) {
+		val[i] = INF;
 	}
 
-	for (int i = 0; i < M; ++i) {
-		int num;
-		cin >> num;
-		for (int j = 0; j < num; ++j) {
-			int people;
-			cin >> people;
-			party[i].emplace_back(people);
-			if (j != 0) {
-				Union(party[i][j], party[i][j-1]);
-			}
-		}
+	int K;
+	cin >> K;
+
+	for (int i = 0; i < E; ++i) {
+		int start, end, weight;
+		cin >> start >> end >> weight;
+		v[start].emplace_back(weight, end);
 	}
-	int count = 0;
-	for (int i = 0; i < M; ++i) {
-		if (Find(party[i][0]) != 0)count++;
+	dijkstra(K);
+	for (int i = 1; i <= V; ++i) {
+		if (val[i] == 1e9)
+			cout << "INF\n";
+		else
+			cout << val[i] << "\n";
 	}
-	cout << count;
 }	
  
