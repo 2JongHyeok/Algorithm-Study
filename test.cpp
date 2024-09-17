648fbe23bce24017f3f109eac92c4e6a5bbdc5d3
2JongHyeok <gurpaper@naver.com>
2024-09-17 20:09:42 +0900

    1967번 트리의 지름

--- test.cpp
+++ test.cpp
@@ -1,7 +1,27 @@
 #include <iostream>
-#include <string>
+#include <vector>
 
 using namespace std;
+vector<vector<pair<int, int>>> v(10001, vector<pair<int, int>>());
+int max_distance, farthest_node;
+
+void DFS(int start, int parent, int distance) {
+	if (max_distance < distance) {
+		max_distance = distance;
+		farthest_node = start;
+	}
+	for (auto c : v[start]) {
+		if (c.first == parent) continue;
+		DFS(c.first, start, distance + c.second);
+	}
+}
+
+void Find(int start) {
+	DFS(start, -1, 0);
+	int start_node = farthest_node;
+	max_distance = 0;
+	DFS(start_node, -1, 0);
+}
 
 int main()
 {
@@ -9,7 +29,14 @@ int main()
 	std::cin.tie(NULL);
 	std::cout.tie(NULL);
 
-	string s1, s2;
-	cin >> s1 >> s2;
-	if (s1.size() >= s2.size() ? cout << "go" : cout << "no");
-}	
+	int N;
+	cin >> N;
+	for (int i = 0; i < N - 1; ++i) {
+		int start, end, weight;
+		cin >> start >> end >> weight;
+		v[start].emplace_back(make_pair(end, weight));
+		v[end].emplace_back(make_pair(start, weight));
+	}
+	Find(1);
+	cout << max_distance;
+}
