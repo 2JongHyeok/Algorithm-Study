6f91206c247d5e8496a70e4f215a5b1fc9d8c3b6
2JongHyeok <gurpaper@naver.com>
2024-12-07 03:05:31 +0900

    3015번 오아시스 재결합

--- test.cpp
+++ test.cpp
@@ -1,43 +1,48 @@
 #include <iostream>
-#include <vector>
+#include <stack>
 
 using namespace std;
-int col[15];
-int total = 0;
-int N;
 
-bool check(int n)
-{
-	for (int i = 0; i < n; ++i) {
-		if (col[i] == col[n] || abs(col[n] - col[i]) == n - i) {
-			return false;
-		}
-	}
-	return true;
-}
-
-void Find(int count)
-{
-	if (count == N) {
-		total++;
-		return;
-	}
-	for (int i = 0; i < N; ++i) {
-		col[count] = i;
-		if (check(count)) {
-			Find(count + 1);
-		}
-	}
-}
-
+int peoples[500'000];
+int total;
+int answer;
 int main()
 {
 	std::ios::sync_with_stdio(false);
 	std::cin.tie(NULL);
 	std::cout.tie(NULL);
 	
-	cin >> N;
+	cin >> total;
 
-	Find(0);
-	cout << total;
+	for (int i = 0; i < total; ++i) {
+		cin >> peoples[i];
+	}
+	
+	stack<pair<int,int>> s;
+
+	s.push(make_pair(peoples[0],0));
+
+	for (int i = 1; i < total; ++i) {
+		while (!s.empty()) {
+			if (s.top().first < peoples[i]) {
+				answer += s.top().second + 1;
+				s.pop();
+			}
+			else if (s.top().first == peoples[i]) {
+				int can_see = s.top().second;
+				s.push(make_pair(peoples[i], can_see + 1));
+				break;
+			}
+			else {
+				s.push(make_pair(peoples[i], 1));
+				break;
+			}
+		}
+		if (s.empty()) s.push(make_pair(peoples[i], 0));
+	}
+	while (!s.empty()) {
+		answer+=s.top().second;
+		s.pop();
+	}
+	cout << answer;
 }
