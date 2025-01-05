e97e253836e9e107cf87390eb0a8b8f113516af7
Lee Jong Hyeok <gurpaper@naver.com>
2025-01-05 21:25:44 +0900

    9935번 문자열 폭발
    string s; 에 대하여
    s = something + s; 같은 연산은
    시간 복잡도가 늘어나니 사용 하지 말자
    차라리 s+=somthing 을 한 뒤
    reverse(s.begin(), s.end())를 하자

--- test.cpp
+++ test.cpp
@@ -1,48 +1,47 @@
 #include <iostream>
 #include <stack>
+#include <string>
+#include <algorithm>
 
 using namespace std;
 
-int peoples[500'000];
-int total;
-int answer;
 int main()
 {
 	std::ios::sync_with_stdio(false);
 	std::cin.tie(NULL);
 	std::cout.tie(NULL);
 	
-	cin >> total;
-
-	for (int i = 0; i < total; ++i) {
-		cin >> peoples[i];
-	}
-	
-	stack<pair<int,int>> s;
-
-	s.push(make_pair(peoples[0],0));
-
-	for (int i = 1; i < total; ++i) {
-		while (!s.empty()) {
-			if (s.top().first < peoples[i]) {
-				answer += s.top().second + 1;
+	string given, wants;
+	cin >> given >> wants;
+	int wants_length = wants.size();
+	reverse(wants.begin(), wants.end());
+	stack<char> s;
+	for (int i = 0; i < given.size(); ++i) {
+		s.push(given[i]);
+		if (s.top() == wants[0] && s.size() >= wants_length) {
+			string temp;
+			for (int i = 0; i < wants_length; ++i) {
+				temp += s.top();
 				s.pop();
 			}
-			else if (s.top().first == peoples[i]) {
-				int can_see = s.top().second;
-				s.push(make_pair(peoples[i], can_see + 1));
-				break;
-			}
-			else {
-				s.push(make_pair(peoples[i], 1));
-				break;
+			if (temp != wants) {
+				for (int i = wants_length-1; i >= 0; --i) {
+					s.push(temp[i]);
+				}
 			}
 		}
-		if (s.empty()) s.push(make_pair(peoples[i], 0));
 	}
-	while (!s.empty()) {
-		answer+=s.top().second;
-		s.pop();
+	string answer;
+	answer.reserve(1000000);
+	if (s.empty()) {
+		cout << "FRULA";
 	}
-	cout << answer;
+	else {
+		while (!s.empty()) {
+			answer += s.top();
+			s.pop();
+		}
+		reverse(answer.begin(), answer.end());
+		cout << answer;
+	}
 }
