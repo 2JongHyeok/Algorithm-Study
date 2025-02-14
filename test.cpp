7abbb30c62aea48e01e86bb17cc197d6c95bee9f
Lee Jong Hyeok <gurpaper@naver.com>
2025-02-14 22:40:12 +0100

    10830번 행렬 제곱

--- test.cpp
+++ test.cpp
@@ -1,9 +1,52 @@
 #include <iostream>
-#include <stack>
-#include <string>
-#include <algorithm>
+#include	<vector>
 
 using namespace std;
+
+class Matrix {
+	int n;
+
+public:
+	vector<vector<int>> mat;
+	Matrix(int n) {
+		this->n = n;
+		mat.resize(n);
+		for (int i = 0; i < n; ++i) mat[i].resize(n);
+	}
+
+	void Print() {
+		for (int i = 0; i < n; ++i) {
+			for (int j = 0; j < n; ++j) {
+				cout << mat[i][j] << " ";
+			}
+			cout << "\n";
+		}
+	}
+	Matrix operator*(const Matrix& a) {
+		Matrix tmp(this->n);
+
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				for (int k = 0; k < n; k++) {
+					tmp.mat[i][j] += this->mat[i][k] * a.mat[k][j];
+				}
+				tmp.mat[i][j] %= 1000;
+			}
+		}
+		return tmp;
+	}
+};
+
+Matrix Pow(const Matrix& a, long long b) {
+	if (b == 1) return a;
+
+	Matrix tmp = Pow(a, b / 2);
+
+	if (b % 2 == 0)
+		return tmp * tmp;
+	else
+		return tmp * tmp * a;
+}
 
 int main()
 {
@@ -11,37 +54,18 @@ int main()
 	std::cin.tie(NULL);
 	std::cout.tie(NULL);
 	
-	string given, wants;
-	cin >> given >> wants;
-	int wants_length = wants.size();
-	reverse(wants.begin(), wants.end());
-	stack<char> s;
-	for (int i = 0; i < given.size(); ++i) {
-		s.push(given[i]);
-		if (s.top() == wants[0] && s.size() >= wants_length) {
-			string temp;
-			for (int i = 0; i < wants_length; ++i) {
-				temp += s.top();
-				s.pop();
-			}
-			if (temp != wants) {
-				for (int i = wants_length-1; i >= 0; --i) {
-					s.push(temp[i]);
-				}
-			}
+	long long N, B;
+	cin >> N >> B;
+
+	Matrix m(N);
+	for (int i = 0; i < N; ++i) {
+		for (int j = 0; j < N; ++j) {
+			cin >> m.mat[i][j];
+			m.mat[i][j] %= 1000;
 		}
 	}
-	string answer;
-	answer.reserve(1000000);
-	if (s.empty()) {
-		cout << "FRULA";
-	}
-	else {
-		while (!s.empty()) {
-			answer += s.top();
-			s.pop();
-		}
-		reverse(answer.begin(), answer.end());
-		cout << answer;
-	}
+
+	Matrix answer = Pow(m, B);
+
+	answer.Print();
 }
