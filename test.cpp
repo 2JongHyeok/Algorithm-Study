a4800f2e8f63bc4e9a55331ac777d6f19c968551
Lee Jong Hyeok <gurpaper@naver.com>
2025-02-16 22:54:06 +0100

    11404 플로이드

--- test.cpp
+++ test.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
-#include	<vector>
+#include <vector>
 
 using namespace std;
+
+#define INF 0x7FFFFFFF
 
 int main()
 {
@@ -9,54 +11,41 @@ int main()
 	std::cin.tie(NULL);
 	std::cout.tie(NULL);
 	
-	vector<int> v;
-	int n;
-	cin >> n;
-	v.resize(n);
-	for (int i = 0; i < n; ++i) cin >> v[i];
-	vector<int> forwardDp;
-	vector<int> backwardDp;
-	vector<int> forwardDpCount;
-	vector<int> backwardDpCount;
-	forwardDp.emplace_back(v[0]);
-	backwardDp.emplace_back(v[n-1]);
-	forwardDpCount.emplace_back(1);
-	backwardDpCount.emplace_back(1);
-
-	for (int i = 1; i < n; ++i) {
-		if (v[i] > forwardDp[forwardDp.size() - 1]) {
-			forwardDp.emplace_back(v[i]);
-			forwardDpCount.emplace_back(forwardDp.size());
-			continue;
+	int N, M;
+	vector<vector<long long>> costs;
+	cin >> N >> M;
+	costs.resize(N);
+	
+	for (int i = 0; i < N; ++i) {
+		for (int j = 0; j < N; ++j) {
+			if (i == j)
+				costs[i].emplace_back(0);
+			else
+				costs[i].emplace_back(INF);
 		}
-		for (int j = 0; j < forwardDp.size(); ++j) {
-			if (v[i] <= forwardDp[j]) {
-				forwardDp[j] = v[i];
-				break;
+	}
+
+	for (int i = 0; i < M; ++i) {
+		int start, end, cost;
+		cin >> start >> end >> cost;
+		if (costs[start - 1][end - 1] > cost) costs[start - 1][end - 1] = cost;
+	}
+
+	for (int i = 0; i < N; ++i) {
+		for (int j = 0; j < N; ++j) {
+			for (int k = 0; k < N; ++k) {
+				if (costs[j][i] + costs[i][k] < costs[j][k]) {
+					costs[j][k] = costs[j][i] + costs[i][k];
+				}
 			}
 		}
-		forwardDpCount.emplace_back(forwardDp.size());
 	}
-	for (int i = n-2; i >=0; --i) {
-		if (v[i] > backwardDp[backwardDp.size() - 1]) {
-			backwardDp.emplace_back(v[i]);
-			backwardDpCount.emplace_back(backwardDp.size());
-			continue;
-		}
-		for (int j = 0; j < backwardDp.size(); ++j) {
-			if (v[i] <=backwardDp[j]) {
-				backwardDp[j] = v[i];
-				break;
-			}
-		}
-		backwardDpCount.emplace_back(backwardDp.size());
 
+	for (int i = 0; i < N; ++i) {
+		for (int j = 0; j < N; ++j) {
+			if (costs[i][j] == INF) cout << "0 ";
+			else cout << costs[i][j] << " ";
+		}
+		cout << "\n";
 	}
-	int max = 0;
-	for (int i = 0; i < n; ++i) {
-		int temp = forwardDpCount[i] + backwardDpCount[n-i-1];
-		if (temp > max)
-			max = temp;
-	}
-	cout << max-1;
 }
