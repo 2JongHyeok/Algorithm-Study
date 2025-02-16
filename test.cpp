6a36a6ab93911bdd960957bb1043e7ea9261d4bd
Lee Jong Hyeok <gurpaper@naver.com>
2025-02-16 22:19:47 +0100

    11054번 가장 긴 바이토닉 부분 수열

--- test.cpp
+++ test.cpp
@@ -3,69 +3,60 @@
 
 using namespace std;
 
-class Matrix {
-	int n;
-
-public:
-	vector<vector<int>> mat;
-	Matrix(int n) {
-		this->n = n;
-		mat.resize(n);
-		for (int i = 0; i < n; ++i) mat[i].resize(n);
-	}
-
-	void Print() {
-		for (int i = 0; i < n; ++i) {
-			for (int j = 0; j < n; ++j) {
-				cout << mat[i][j] << " ";
-			}
-			cout << "\n";
-		}
-	}
-	Matrix operator*(const Matrix& a) {
-		Matrix tmp(this->n);
-
-		for (int i = 0; i < n; i++) {
-			for (int j = 0; j < n; j++) {
-				for (int k = 0; k < n; k++) {
-					tmp.mat[i][j] += this->mat[i][k] * a.mat[k][j];
-				}
-				tmp.mat[i][j] %= 1000;
-			}
-		}
-		return tmp;
-	}
-};
-
-Matrix Pow(const Matrix& a, long long b) {
-	if (b == 1) return a;
-
-	Matrix tmp = Pow(a, b / 2);
-
-	if (b % 2 == 0)
-		return tmp * tmp;
-	else
-		return tmp * tmp * a;
-}
-
 int main()
 {
 	std::ios::sync_with_stdio(false);
 	std::cin.tie(NULL);
 	std::cout.tie(NULL);
 	
-	long long N, B;
-	cin >> N >> B;
+	vector<int> v;
+	int n;
+	cin >> n;
+	v.resize(n);
+	for (int i = 0; i < n; ++i) cin >> v[i];
+	vector<int> forwardDp;
+	vector<int> backwardDp;
+	vector<int> forwardDpCount;
+	vector<int> backwardDpCount;
+	forwardDp.emplace_back(v[0]);
+	backwardDp.emplace_back(v[n-1]);
+	forwardDpCount.emplace_back(1);
+	backwardDpCount.emplace_back(1);
 
-	Matrix m(N);
-	for (int i = 0; i < N; ++i) {
-		for (int j = 0; j < N; ++j) {
-			cin >> m.mat[i][j];
-			m.mat[i][j] %= 1000;
+	for (int i = 1; i < n; ++i) {
+		if (v[i] > forwardDp[forwardDp.size() - 1]) {
+			forwardDp.emplace_back(v[i]);
+			forwardDpCount.emplace_back(forwardDp.size());
+			continue;
 		}
+		for (int j = 0; j < forwardDp.size(); ++j) {
+			if (v[i] <= forwardDp[j]) {
+				forwardDp[j] = v[i];
+				break;
+			}
+		}
+		forwardDpCount.emplace_back(forwardDp.size());
 	}
+	for (int i = n-2; i >=0; --i) {
+		if (v[i] > backwardDp[backwardDp.size() - 1]) {
+			backwardDp.emplace_back(v[i]);
+			backwardDpCount.emplace_back(backwardDp.size());
+			continue;
+		}
+		for (int j = 0; j < backwardDp.size(); ++j) {
+			if (v[i] <=backwardDp[j]) {
+				backwardDp[j] = v[i];
+				break;
+			}
+		}
+		backwardDpCount.emplace_back(backwardDp.size());
 
-	Matrix answer = Pow(m, B);
-
-	answer.Print();
+	}
+	int max = 0;
+	for (int i = 0; i < n; ++i) {
+		int temp = forwardDpCount[i] + backwardDpCount[n-i-1];
+		if (temp > max)
+			max = temp;
+	}
+	cout << max-1;
 }
