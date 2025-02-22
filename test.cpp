b79d97d3220692c958c5c1d2416cfffc33b5dc29
Lee Jong Hyeok <gurpaper@naver.com>
2025-02-22 01:22:33 +0100

    17144번 미세먼지 안녕!
    술먹고 풀어서 시간이 좀 걸렸던 문제
    문제 자체는 쉬우나 노가다가 좀 많았다.
    노가다를 하지 않는방법도 있지 않을까..?

--- test.cpp
+++ test.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 #include <vector>
-
-#define MAX  0x7FFFFFFF
+#include <queue>
 
 using namespace std;
 
@@ -11,53 +10,83 @@ int main()
 	std::cin.tie(NULL);
 	std::cout.tie(NULL);
 	
+	int R, C, T;
+	cin >> R >> C >> T;
+
 	vector<vector<long long>> map;
-	vector<int> items;
-	int n, m, r;
-	cin >> n >> m >> r;
+	
+	map.resize(R);
 
-	map.resize(n);
-	for (int i = 0; i < n; ++i)
-		for (int j = 0; j < n; ++j)
-			map[i].emplace_back(MAX);
+	int air_purifier=2;
 
-
-	for (int i = 0; i < n; ++i) {
-		int tmp;
-		cin >> tmp;
-		items.emplace_back(tmp);
+	for (int i = 0; i < R; ++i) {
+		for (int j = 0; j < C; ++j) {
+			int tmp;
+			cin >> tmp;
+			map[i].emplace_back(tmp);
+			if (tmp == -1) air_purifier = i - 1;
+		}
 	}
 
-	for (int i = 0; i < r; ++i) {
-		int start, end, val;
-		cin >> start >> end >> val;
-		map[start-1][end-1] = val;
-		map[end-1][start-1] = val;
-	}
+	int dirX[4] = {0,0,1,-1};
+	int dirY[4] = {1,-1,0,0};
 
-	for (int i = 0; i < n; ++i) map[i][i] = 0;
-
-	for (int i = 0; i < n; ++i) {
-		for (int j = 0; j < n; ++j) {
-			for (int k = 0; k < n; ++k) {
-				if (map[j][i] + map[i][k] < map[j][k]) {
-					map[j][k] = map[j][i] + map[i][k];
+	queue<pair<pair<int, int>,long long>> diffusion;
+	for (int time = 0; time < T; ++time) {
+		for (int i = 0; i < R; ++i) {
+			for (int j = 0; j < C; ++j) {
+				if (map[i][j] >= 5) {
+					int count = 0;
+					for (int dir = 0; dir < 4; ++dir) {
+						int tmpX = j+dirX[dir], tmpY = i+dirY[dir];
+						if (tmpX < 0 || tmpX >= C || tmpY < 0 || tmpY >= R || map[tmpY][tmpX] == -1) continue;
+						diffusion.push(make_pair(make_pair(tmpY, tmpX), map[i][j] / 5));
+						count++;
+					}
+					map[i][j] -= map[i][j] / 5 * count;
 				}
 			}
 		}
-	}
-
-	int answer = 0;
-
-	for (int i = 0; i < n; ++i) {
-		int temp = 0;
-		for (int j = 0; j < n; ++j) {
-			if (map[i][j] <= m)
-				temp += items[j];
+		while (!diffusion.empty()) {
+			pair<pair<int, int>, int> tmp = diffusion.front();
+			diffusion.pop();
+			map[tmp.first.first][tmp.first.second] += tmp.second;
 		}
-		if (temp > answer)
-			answer = temp;
+
+		for (int i = air_purifier - 1; i > 0; --i) {
+			map[i][0] = map[i - 1][0];
+		}
+		for (int i = 0; i < C-1; ++i) {
+			map[0][i] = map[0][i + 1];
+		}
+		for (int i = 0; i < air_purifier; ++i) {
+			map[i][C-1] = map[i + 1][C-1];
+		}
+		for (int i = C - 1; i > 1; --i) {
+			map[air_purifier][i] = map[air_purifier][i - 1];
+		}
+		map[air_purifier][1] = 0;
+		for (int i = air_purifier + 2; i < R-1; ++i) {
+			map[i][0] = map[i+1][0];
+		}
+		for (int i = 0; i < C - 1; ++i) {
+			map[R - 1][i] = map[R - 1][i + 1];
+		}
+		for (int i = R - 1; i > air_purifier+1; --i) {
+			map[i][C - 1] = map[i - 1][C - 1];
+		}
+		for (int i = C - 1; i > 1; --i) {
+			map[air_purifier + 1][i] = map[air_purifier + 1][i - 1];
+		}
+		map[air_purifier + 1][1] = 0;
 	}
 
-	cout << answer;
+	long long answer = 0;
+	for (int i = 0; i < R; ++i) {
+		for (int j = 0; j < C; ++j) {
+			answer += map[i][j];
+		}
+	}
+
+	cout << answer + 2;
 }
