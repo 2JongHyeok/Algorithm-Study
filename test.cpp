abe014234a2b6b5c9af380d5f58b4721f56c0e01
Lee Jong Hyeok <gurpaper@naver.com>
2025-03-19 20:11:49 +0900

    30805번 사전 순 최대 공통 부분 수열

--- test.cpp
+++ test.cpp
@@ -4,89 +4,84 @@
 
 using namespace std;
 
+struct cmp {
+	bool operator()(const pair<int, int>& a, const pair<int, int>& b) const {
+		if (a.first != b.first)
+			return a.first < b.first;
+		return a.second > b.second;
+	}
+};
+
 int main()
 {
 	std::ios::sync_with_stdio(false);
 	std::cin.tie(NULL);
 	std::cout.tie(NULL);
-	
-	int R, C, T;
-	cin >> R >> C >> T;
 
-	vector<vector<long long>> map;
-	
-	map.resize(R);
+	int N, M;
+	cin >> N;
 
-	int air_purifier=2;
+	vector<int> arr1, arr2;
 
-	for (int i = 0; i < R; ++i) {
-		for (int j = 0; j < C; ++j) {
-			int tmp;
-			cin >> tmp;
-			map[i].emplace_back(tmp);
-			if (tmp == -1) air_purifier = i - 1;
-		}
+	for (int i = 0; i < N; ++i) {
+		int tmp;
+		cin >> tmp;
+		arr1.emplace_back(tmp);
 	}
+	cin >> M;
+	for (int i = 0; i < M; ++i) {
+		int tmp;
+		cin >> tmp;
+		arr2.emplace_back(tmp);
+	}
+	vector<int> answer;
+	int max1_index = -1;
+	int max2_index = -1;
+	while (true) {
+		priority_queue<pair<int, int>,vector<pair<int,int>>, cmp> pq1, pq2;
+		int max1 = -1;
+		int	max2 = -1;
+		for (int i = max1_index + 1; i < arr1.size(); ++i) {
+			pq1.push(make_pair(arr1[i], i));
+		}
+		for (int i = max2_index + 1; i < arr2.size(); ++i) {
+			pq2.push(make_pair(arr2[i], i));
+		}
 
-	int dirX[4] = {0,0,1,-1};
-	int dirY[4] = {1,-1,0,0};
+		if (!pq1.empty()) {
+			max1 = pq1.top().first;
+			max1_index = pq1.top().second;
+		}
+		if (!pq2.empty()) {
+			max2 = pq2.top().first;
+			max2_index = pq2.top().second;
+		}
 
-	queue<pair<pair<int, int>,long long>> diffusion;
-	for (int time = 0; time < T; ++time) {
-		for (int i = 0; i < R; ++i) {
-			for (int j = 0; j < C; ++j) {
-				if (map[i][j] >= 5) {
-					int count = 0;
-					for (int dir = 0; dir < 4; ++dir) {
-						int tmpX = j+dirX[dir], tmpY = i+dirY[dir];
-						if (tmpX < 0 || tmpX >= C || tmpY < 0 || tmpY >= R || map[tmpY][tmpX] == -1) continue;
-						diffusion.push(make_pair(make_pair(tmpY, tmpX), map[i][j] / 5));
-						count++;
-					}
-					map[i][j] -= map[i][j] / 5 * count;
-				}
+		int finished = true;
+
+		while (!pq1.empty() && !pq2.empty()) {
+			if (max1 > max2) {
+				max1 = pq1.top().first;
+				max1_index = pq1.top().second;
+				pq1.pop();
+			}
+			else if (max2 > max1) {
+				max2 = pq2.top().first;
+				max2_index = pq2.top().second;
+				pq2.pop();
+			}
+			if (max1 == max2) {
+				finished = false;
+				answer.emplace_back(max1);
+				break;
 			}
 		}
-		while (!diffusion.empty()) {
-			pair<pair<int, int>, int> tmp = diffusion.front();
-			diffusion.pop();
-			map[tmp.first.first][tmp.first.second] += tmp.second;
-		}
-
-		for (int i = air_purifier - 1; i > 0; --i) {
-			map[i][0] = map[i - 1][0];
-		}
-		for (int i = 0; i < C-1; ++i) {
-			map[0][i] = map[0][i + 1];
-		}
-		for (int i = 0; i < air_purifier; ++i) {
-			map[i][C-1] = map[i + 1][C-1];
-		}
-		for (int i = C - 1; i > 1; --i) {
-			map[air_purifier][i] = map[air_purifier][i - 1];
-		}
-		map[air_purifier][1] = 0;
-		for (int i = air_purifier + 2; i < R-1; ++i) {
-			map[i][0] = map[i+1][0];
-		}
-		for (int i = 0; i < C - 1; ++i) {
-			map[R - 1][i] = map[R - 1][i + 1];
-		}
-		for (int i = R - 1; i > air_purifier+1; --i) {
-			map[i][C - 1] = map[i - 1][C - 1];
-		}
-		for (int i = C - 1; i > 1; --i) {
-			map[air_purifier + 1][i] = map[air_purifier + 1][i - 1];
-		}
-		map[air_purifier + 1][1] = 0;
+		if (finished) break;
 	}
-
-	long long answer = 0;
-	for (int i = 0; i < R; ++i) {
-		for (int j = 0; j < C; ++j) {
-			answer += map[i][j];
-		}
+	if (answer.size() == 0) cout << 0;
+	else {
+		cout << answer.size() << "\n";
+		for (int i = 0; i < answer.size(); ++i)
+			cout << answer[i] << " ";
 	}
-
-	cout << answer + 2;
-}
+}	
