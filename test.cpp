3d620864cf19d3a4be87814bc787d73250ff70e7
2JongHyeok <gurpaper@naver.com>
2025-04-25 23:39:40 +0900

    1865번 웜홀

--- test.cpp
+++ test.cpp
@@ -1,31 +1,34 @@
 #include <iostream>
 #include <vector>
-#include <queue>
+#include <tuple>
 #include <limits>
 
 using namespace std;
 constexpr int INF = numeric_limits<int>::max();
 
-int N, M, X;
+int N, M, W;
 
-void dijkstra(const int start, const vector<vector<pair<int, int>>>& graph, vector<int>& dist) {
-	dist.assign(N + 1, INF);
-	dist[start ] = 0;
+bool bellman_ford(const vector<tuple<int,int,int>>& edges)
+{
+	vector<int> dist(N + 1, INF);
 
-	priority_queue<pair<int, int>, vector<pair<int, int>>, greater<>> pq;
-	pq.push({ 0, start });
-
-	while (!pq.empty()) {
-		auto [curr_dist, u] = pq.top(); pq.pop();
-		if (curr_dist > dist[u]) continue;
-
-		for (auto [v, weight] : graph[u]) {
-			if (dist[v] > dist[u] + weight) {
-				dist[v] = dist[u] + weight;
-				pq.push({ dist[v],v });
+	dist[0] = 0;
+	
+	for (int i = 0; i < N; ++i) {
+		for (auto [u, v, w] : edges) {
+			if (dist[u] != INF && dist[v] > dist[u] + w) {
+				dist[v] = dist[u] + w;
 			}
 		}
 	}
+
+	for (auto [u, v, w] : edges) {
+		if (dist[u] != INF && dist[v] > dist[u] + w) {
+			return true;
+		}
+	}
+	return false;
+
 }
 
 int main()
@@ -33,29 +36,34 @@ int main()
 	std::ios::sync_with_stdio(false);
 	std::cin.tie(NULL);
 	std::cout.tie(NULL);
-
-	cin >> N >> M >> X;
-
-	vector<vector<pair<int, int>>> map(N+1);
-	vector<vector<pair<int, int>>> rev_map(N+1);
 	
-	for (int i = 0; i < M; ++i) {	// 시간 초기화
-		int u, v, w;
-		cin >> u >> v >> w;
-		map[u].emplace_back(v, w);
-		rev_map[v].emplace_back(u, w);
+	int TC;
+	cin >> TC;
+
+	for (int tc = 0; tc < TC; ++tc) {
+		cin >> N >> M >> W;
+		vector<tuple<int, int, int>> edges;
+		for (int i = 0; i < M; ++i) {
+			int u, v, w;
+			cin >> u >> v >> w;
+			edges.emplace_back( u,v,w );
+			edges.emplace_back( v,u,w );
+		}
+		for (int i = 0; i < W; ++i) {
+			int u, v, w;
+			cin >> u >> v >> w;
+			edges.emplace_back(u, v, -w);
+		}
+		for (int i = 1; i <= N; ++i) {
+			edges.emplace_back(0, i, 0);
+		}
+		if (bellman_ford(edges)) {
+			cout << "YES\n";
+		}
+		else {
+			cout << "NO\n";
+		}
 	}
 
-	vector<int> toX(N + 1), fromX(N + 1);
-	dijkstra(X, map, fromX);
-	dijkstra(X, rev_map, toX);
-
-	int max_time = 0;
-	for (int i = 1; i <= N; ++i) {
-		int round = toX[i] + fromX[i];
-		max_time = max(max_time, round);
-	}
-
-	cout << max_time;
 	return 0;
 }
