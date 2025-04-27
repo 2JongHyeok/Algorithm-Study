032bee0d6e6efd78a95c5e22253c4064da20b64d
2JongHyeok <gurpaper@naver.com>
2025-04-28 02:11:50 +0900

    2638번 치즈

--- test.cpp
+++ test.cpp
@@ -7,8 +7,78 @@
 using namespace std;
 
 int N, M;
+vector<vector<int>> map;
+bool visited[100][100];
+struct pos
+{
+	int x, y;
+};
+struct info
+{
+	pos p;
+	int count;
+};
+queue<info> cheese;
 
-bool visited[1000][1000][2];	// 방문여부 + 벽을 부쉈는지 안부쉈는지
+int BFS()
+{
+	// step 1 : 바깥공기 찾기
+	queue<pos> q;
+	q.push({ 0,0 });
+	visited[0][0] = true;
+	int X[4]{ 1,-1,0,0 };
+	int Y[4]{ 0,0,1,-1 };
+	while (!q.empty()) {
+		auto [x, y] = q.front(); q.pop();
+		for (int i = 0; i < 4; ++i) {
+			int newX = x + X[i];
+			int newY = y + Y[i];
+			if (newX < 0 || newX >= M || newY < 0 || newY >= N) continue;
+			if (map[newY][newX] == 1) continue;
+			if (visited[newY][newX]) continue;
+			visited[newY][newX] = true;
+			q.push({ newX, newY });
+		}
+	}
+
+	// step 2 : 치즈 녹이기
+	int finishTime = 0;
+	while (!cheese.empty()) {
+		auto [pos, time] = cheese.front(); cheese.pop();
+		int count = 0;
+		if (finishTime != time) {
+			finishTime++;
+			while (!q.empty()) {
+				auto [x, y] = q.front(); q.pop();
+				map[y][x] = 0;
+				visited[y][x] = true;
+				for (int i = 0; i < 4; ++i) {
+					int newX = x + X[i];
+					int newY = y + Y[i];
+					if (newX < 0 || newX >= M || newY < 0 || newY >= N) continue;
+					if (map[newY][newX] == 1) continue;
+					if (visited[newY][newX]) continue;
+					visited[newY][newX] = true;
+					q.push({ newX, newY });
+				}
+			}
+		}
+		for (int i = 0; i < 4; ++i) {
+			int newX = pos.x + X[i];
+			int newY = pos.y + Y[i];
+			if (newX < 0 || newX >= M || newY < 0 || newY >= N) continue;
+			if (visited[newY][newX])
+				count++;
+		}
+		if (count >= 2) {
+			q.push(pos);
+		}
+		else {
+			cheese.push({ pos,time + 1 });
+		}
+	}
+	return finishTime+1;
+}
 
 int main()
 {
@@ -17,64 +87,16 @@ int main()
 	std::cout.tie(NULL);
 	
 	cin >> N >> M;
-	vector<vector<char>> map(N, vector<char>(M, 0));
-
+	map.resize(N, vector<int>(M));
 
 	for (int i = 0; i < N; ++i) {
-		string s;
-		cin >> s;
 		for (int j = 0; j < M; ++j) {
-			map[i][j] = s[j];
+			cin >> map[i][j];
+			if (map[i][j] == 1)
+				cheese.push({ j,i,0 });
 		}
 	}
 
-	if (N == 1 && M == 1) {
-		cout << 1;
-		return 0;
-	}
-	queue <tuple<int, int, int, bool>> q;
-
-	q.push({ 1,0, 0, false });
-	
-	int X[4]{ 1,0,0,-1 };
-	int Y[4]{ 0,1,-1,0 };
-	bool found = false;
-	while (!q.empty()) {
-		auto [time, x, y, broken] = q.front(); q.pop();
-		for (int i = 0; i < 4; ++i) {
-			int newX = x + X[i], newY = y+Y[i];
-			if (newX < 0 || newX >= M || newY < 0 || newY >= N) continue;
-			if (newX == M - 1 && newY == N - 1) {
-				found = true;
-				cout << time + 1;
-				break;
-			}
-			if (broken) {
-				if (map[newY][newX] == '1') continue;
-				if (visited[newY][newX][0]) continue;
-				visited[newY][newX][0] = true;
-				visited[newY][newX][1] = true;
-				q.push({ time + 1, newX, newY, broken });
-			}
-			else {
-				if (visited[newY][newX][0] && !visited[newY][newX][1]) continue;
-				if (map[newY][newX] == '1') {
-					q.push({ time + 1, newX, newY, true });
-					
-				}
-				else {
-					q.push({ time + 1, newX, newY, false });
-				}
-				visited[newY][newX][0] = true;
-				visited[newY][newX][1] = false;
-			}
-		}
-		if (found)
-			break;
-	}
-	if (!found) {
-		cout << -1;
-	}
-
-	return 0;
+	int time = BFS();
+	cout << time;
 }
